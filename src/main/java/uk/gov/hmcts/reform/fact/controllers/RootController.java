d342676b05b765fdaee0896e96c13a70d23ed252
velimd <duzgunce.vd@gmail.com>
2020-09-10 14:35:09 +0100

    cleaning up

--- src/main/java/uk/gov/hmcts/reform/fact/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/fact/controllers/RootController.java
@@ -23,6 +23,6 @@ public class RootController {
      */
     @GetMapping("/")
     public ResponseEntity<String> welcome() {
-        return ok("Welcome to fact-backend");
+        return ok("Welcome to fact-api");
     }
 }
