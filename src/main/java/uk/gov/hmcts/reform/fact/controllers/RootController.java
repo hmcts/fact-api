1e13ad529a3d83d00157e9a10afd833c38b5efbd
velimd <duzgunce.vd@gmail.com>
2020-09-11 12:37:29 +0100

    testing pr build

--- src/main/java/uk/gov/hmcts/reform/fact/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/fact/controllers/RootController.java
@@ -23,6 +23,6 @@ public class RootController {
      */
     @GetMapping("/")
     public ResponseEntity<String> welcome() {
-        return ok("Welcome to fact-api");
+        return ok("Welcome to FACT API");
     }
 }
