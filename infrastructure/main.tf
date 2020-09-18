7cc496e245c6417ecb1b7a9d26a65d9a0771100c
Linus Norton <linusnorton@gmail.com>
2020-09-18 08:50:25 +0100

    change rg name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,7 +9,7 @@ locals {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.component}-${var.env}-rg"
+  name     = "${var.product}-${var.component}-${var.env}"
   location = var.location
 
   tags = var.common_tags
