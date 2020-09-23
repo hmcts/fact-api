d8bc1c102d8dd8c282dcbbc553ecd5e51a921cb7
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-09-23 11:44:05 +0100

    CTF-51: Remove flyway_schema_history

--- src/main/resources/db/migration/V001__Init.sql
+++ src/main/resources/db/migration/V001__Init.sql
@@ -47,26 +47,6 @@ SET row_security = off;
 SET default_tablespace = '';
 
 --
--- Name: flyway_schema_history; Type: TABLE; Schema: public; Owner: fact
---
-
-CREATE TABLE public.flyway_schema_history (
-    installed_rank integer NOT NULL,
-    version character varying(50),
-    description character varying(200) NOT NULL,
-    type character varying(20) NOT NULL,
-    script character varying(1000) NOT NULL,
-    checksum integer,
-    installed_by character varying(100) NOT NULL,
-    installed_on timestamp without time zone DEFAULT now() NOT NULL,
-    execution_time integer NOT NULL,
-    success boolean NOT NULL
-);
-
-
-ALTER TABLE public.flyway_schema_history OWNER TO fact;
-
---
 -- Name: search_addresstype; Type: TABLE; Schema: public; Owner: fact
 --
 
