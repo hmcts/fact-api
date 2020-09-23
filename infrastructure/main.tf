9a73908f0a907e2b7ddd43af5c9fd70068074aca
Linus Norton <linusnorton@gmail.com>
2020-09-23 15:45:04 +0100

    update secret name to match migration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,31 +17,31 @@ provider "vault" {
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
-  name         = "fact-api-POSTGRES-USER"
+  name         = "api-POSTGRES-USER"
   value        = module.fact-database.user_name
   key_vault_id = data.azurerm_key_vault.fact_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
-  name         = "fact-api-POSTGRES-PASS"
+  name         = "api-POSTGRES-PASS"
   value        = module.fact-database.postgresql_password
   key_vault_id = data.azurerm_key_vault.fact_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
-  name         = "fact-api-POSTGRES-HOST"
+  name         = "api-POSTGRES-HOST"
   value        = module.fact-database.host_name
   key_vault_id = data.azurerm_key_vault.fact_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
-  name         = "fact-api-POSTGRES-PORT"
+  name         = "api-POSTGRES-PORT"
   value        = module.fact-database.postgresql_listen_port
   key_vault_id = data.azurerm_key_vault.fact_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
-  name         = "fact-api-POSTGRES-DATABASE"
+  name         = "api-POSTGRES-DATABASE"
   value        = module.fact-database.postgresql_database
   key_vault_id = data.azurerm_key_vault.fact_key_vault.id
 }
