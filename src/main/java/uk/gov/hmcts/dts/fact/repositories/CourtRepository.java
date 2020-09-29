e345973d586281efcc7209dcc58ce0b990e64207
velimd <duzgunce.vd@gmail.com>
2020-09-29 16:21:28 +0100

    clean up

--- src/main/java/uk/gov/hmcts/dts/fact/repositories/CourtRepository.java
+++ src/main/java/uk/gov/hmcts/dts/fact/repositories/CourtRepository.java
@@ -12,7 +12,8 @@ public interface CourtRepository extends JpaRepository<Court, Integer> {
 
     @Query("SELECT c FROM Court c INNER JOIN c.addresses ca " +
         "WHERE LOWER(c.name) LIKE LOWER(concat('%', :query,'%')) " +
-        "OR LOWER(ca.address) LIKE LOWER(concat('%', :query,'%')) OR REPLACE(LOWER(ca.postcode), ' ', '') LIKE REPLACE(LOWER(concat('%', :query,'%')), ' ', '')" +
-        "OR LOWER(ca.townName) = LOWER(concat('%', :query,'%'))")
+        "OR LOWER(ca.address) LIKE LOWER(concat('%', :query,'%')) " +
+        "OR REPLACE(LOWER(ca.postcode), ' ', '') LIKE REPLACE(LOWER(concat('%', :query,'%')), ' ', '')" +
+        "OR LOWER(ca.townName) = LOWER(:query)")
     List<Court> queryBy(String query);
 }
