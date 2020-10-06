30e4894dd905c520cc0eff51a395849d285819cd
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-10-06 13:40:42 +0100

    Remove flyway

--- src/main/resources/db/migration/V002__earthdistance.sql
+++
@@ -1,5 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS cube WITH SCHEMA public;
-COMMENT ON EXTENSION cube IS 'data type for multidimensional cubes';
-
-CREATE EXTENSION IF NOT EXISTS earthdistance WITH SCHEMA public;
-COMMENT ON EXTENSION earthdistance IS 'calculate great-circle distances on the surface of the Earth';
