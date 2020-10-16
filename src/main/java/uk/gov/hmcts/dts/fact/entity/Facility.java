e34d9194c270ba21a6d59ba55ce49fab4baaed04
SarahLittlejohn <sarahjanelittlejohn@gmail.com>
2020-10-16 16:54:23 +0100

    CTF-45: unit tests.

--- src/main/java/uk/gov/hmcts/dts/fact/entity/Facility.java
+++ src/main/java/uk/gov/hmcts/dts/fact/entity/Facility.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.dts.fact.entity;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -9,6 +10,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "search_facility")
 @Getter
+@Setter
 public class Facility {
     @Id
     private Integer id;
