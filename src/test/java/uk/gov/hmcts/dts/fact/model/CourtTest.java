8ae6ac504e8ed59324d25e0a9eb5487e28dbf32a
SarahLittlejohn <sarahjanelittlejohn@gmail.com>
2020-10-19 14:46:45 +0100

    CTF-45: pmd and checkstyle.

--- src/test/java/uk/gov/hmcts/dts/fact/model/CourtTest.java
+++ src/test/java/uk/gov/hmcts/dts/fact/model/CourtTest.java
@@ -1,16 +1,20 @@
 package uk.gov.hmcts.dts.fact.model;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-import uk.gov.hmcts.dts.fact.entity.*;
+import uk.gov.hmcts.dts.fact.entity.AddressType;
 import uk.gov.hmcts.dts.fact.entity.AreaOfLaw;
 import uk.gov.hmcts.dts.fact.entity.Contact;
 import uk.gov.hmcts.dts.fact.entity.CourtAddress;
 import uk.gov.hmcts.dts.fact.entity.CourtEmail;
+import uk.gov.hmcts.dts.fact.entity.CourtType;
 import uk.gov.hmcts.dts.fact.entity.Facility;
+import uk.gov.hmcts.dts.fact.entity.InPerson;
 import uk.gov.hmcts.dts.fact.entity.OpeningTime;
+import uk.gov.hmcts.dts.fact.entity.ServiceArea;
 
 import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CourtTest {
 
@@ -43,7 +47,7 @@ public class CourtTest {
 
         final Contact contactEntity = new Contact();
         contactEntity.setName("DX");
-        contactEntity.setNumber("1234567");
+        contactEntity.setNumber("123");
         contactEntity.setExplanation("Explanation of contact");
         contactEntity.setSortOrder(1);
         courtEntity.setContacts(Collections.singletonList(contactEntity));
@@ -68,40 +72,17 @@ public class CourtTest {
         facilityEntity.setName("Facility");
         courtEntity.setFacilities(Collections.singletonList(facilityEntity));
 
-        courtEntity.setName("Test Court");
-        courtEntity.setSlug("test-court");
         courtEntity.setInfo("<p>Info on court</p>");
-        courtEntity.setDirections("Directions for court");
-        courtEntity.setLat(123456.2345);
-        courtEntity.setLon(123456.1234);
-        courtEntity.setAlert("Alert for court");
-        courtEntity.setCciCode(12345);
-        courtEntity.setNumber(12345);
-        courtEntity.setMagistrateCode(12345);
-        courtEntity.setGbs("Gbd for court");
-        courtEntity.setDisplayed(true);
-        courtEntity.setImageFile("imagefile");
 
         Court court = new Court(courtEntity);
-        assertEquals(courtEntity.getName(), court.getName());
-        assertEquals(courtEntity.getSlug(), court.getSlug());
-        assertEquals("Info on court", court.getInfo());
-        assertEquals(courtEntity.getDirections(), court.getDirections());
-        assertEquals(courtEntity.getLat(), court.getLat());
-        assertEquals(courtEntity.getLon(), court.getLon());
-        assertEquals(courtEntity.getAlert(), court.getAlert());
-        assertEquals(courtEntity.getCciCode(), court.getCountyLocationCode());
-        assertEquals(courtEntity.getNumber(), court.getCrownLocationCode());
-        assertEquals(courtEntity.getMagistrateCode(), court.getMagistratesLocationCode());
-        assertEquals(courtEntity.getGbs(), court.getGbs());
-        assertEquals(courtEntity.getDisplayed(), court.getOpen());
-        assertEquals(courtEntity.getImageFile(), court.getImageFile());
 
+        assertEquals("Info on court", court.getInfo());
         assertEquals(courtEntity.getInPerson().getIsInPerson(), court.getInPerson());
         assertEquals("Visit or contact us", court.getAddresses().get(0).getAddressType());
         assertEquals(courtEntity.getContacts().get(0).getNumber(), court.getDxNumber().get(0));
         assertEquals("Description of facility", court.getFacilities().get(0).getDescription());
         assertEquals(courtEntity.getServiceArea().getService(), court.getServiceArea());
+        assertEquals("Info on court", court.getInfo());
 
     }
 
