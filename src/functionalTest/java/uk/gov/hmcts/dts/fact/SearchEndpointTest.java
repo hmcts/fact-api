487b36fe22722a08377bc9bd31f257b6e12cce13
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-10-22 17:02:09 +0100

    FACT-61: Add functional tests

--- src/functionalTest/java/uk/gov/hmcts/dts/fact/SearchEndpointTest.java
+++ src/functionalTest/java/uk/gov/hmcts/dts/fact/SearchEndpointTest.java
@@ -13,12 +13,15 @@ import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpHeaders.ACCEPT_LANGUAGE;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 @ExtendWith(SpringExtension.class)
 public class SearchEndpointTest {
 
     private static final String CONTENT_TYPE_VALUE = "application/json";
+    private static final String SEARCH_ENDPOINT = "/search/results.json";
+
 
     @Value("${TEST_URL:http://localhost:8080}")
     private String testUrl;
@@ -34,7 +37,7 @@ public class SearchEndpointTest {
             .relaxedHTTPSValidation()
             .header(CONTENT_TYPE, CONTENT_TYPE_VALUE)
             .when()
-            .get("/search/results.json?postcode=OX1 1RZ")
+            .get(SEARCH_ENDPOINT + "?postcode=OX1 1RZ")
             .thenReturn();
 
 
@@ -51,7 +54,7 @@ public class SearchEndpointTest {
             .relaxedHTTPSValidation()
             .header(CONTENT_TYPE, CONTENT_TYPE_VALUE)
             .when()
-            .get("/search/results.json?postcode=OX1 1RZ&aol=" +  aol)
+            .get(SEARCH_ENDPOINT + "?postcode=OX1 1RZ&aol=" + aol)
             .thenReturn();
 
         assertThat(response.statusCode()).isEqualTo(200);
@@ -59,5 +62,63 @@ public class SearchEndpointTest {
         assertThat(courts.size()).isEqualTo(10);
         assertThat(courts).isSortedAccordingTo(Comparator.comparing(CourtWithDistance::getDistance));
         assertThat(courts.stream().allMatch(c -> c.getAreasOfLaw().contains(aol)));
+    }
+
+    @Test
+    public void postcodeSearchShouldSupportWelsh() {
+        final var welshResponse = given()
+            .relaxedHTTPSValidation()
+            .header(CONTENT_TYPE, CONTENT_TYPE_VALUE)
+            .header(ACCEPT_LANGUAGE, "cy")
+            .when()
+            .get(SEARCH_ENDPOINT + "?postcode=CF10 1ET")
+            .thenReturn();
+
+        assertThat(welshResponse.statusCode()).isEqualTo(200);
+        final List<CourtWithDistance> welshCourts = welshResponse.body().jsonPath()
+            .getList(".", CourtWithDistance.class
+            );
+        assertThat(welshCourts.get(0).getAddress().getTownName()).isEqualTo("Caerdydd");
+
+        final var englishResponse = given()
+            .relaxedHTTPSValidation()
+            .header(CONTENT_TYPE, CONTENT_TYPE_VALUE)
+            .when()
+            .get(SEARCH_ENDPOINT + "?postcode=CF10 1ET")
+            .thenReturn();
+
+        assertThat(englishResponse.statusCode()).isEqualTo(200);
+        final List<CourtWithDistance> englishCourts = englishResponse.body().jsonPath()
+            .getList(".", CourtWithDistance.class);
+        assertThat(englishCourts.get(0).getAddress().getTownName()).isEqualTo("Cardiff");
+    }
+
+    @Test
+    public void nameSearchShouldSupportWelsh() {
+        final var welshResponse = given()
+            .relaxedHTTPSValidation()
+            .header(CONTENT_TYPE, CONTENT_TYPE_VALUE)
+            .header(ACCEPT_LANGUAGE, "cy")
+            .when()
+            .get(SEARCH_ENDPOINT + "?q=cardiff") //TODO should be 'caerdydd'
+            .thenReturn();
+
+        assertThat(welshResponse.statusCode()).isEqualTo(200);
+        final List<CourtWithDistance> welshCourts = welshResponse.body().jsonPath()
+            .getList(".", CourtWithDistance.class
+            );
+        assertThat(welshCourts.get(0).getAddress().getTownName()).isEqualTo("Caerdydd");
+
+        final var englishResponse = given()
+            .relaxedHTTPSValidation()
+            .header(CONTENT_TYPE, CONTENT_TYPE_VALUE)
+            .when()
+            .get(SEARCH_ENDPOINT + "?q=cardiff")
+            .thenReturn();
+
+        assertThat(englishResponse.statusCode()).isEqualTo(200);
+        final List<CourtWithDistance> englishCourts = englishResponse.body().jsonPath()
+            .getList(".", CourtWithDistance.class);
+        assertThat(englishCourts.get(0).getAddress().getTownName()).isEqualTo("Cardiff");
     }
 }
