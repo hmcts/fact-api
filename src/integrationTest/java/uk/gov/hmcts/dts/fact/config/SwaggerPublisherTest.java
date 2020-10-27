8389f7fc886aabf01dcdce9c8e33e45dab98a406
Linus Norton <linusnorton@gmail.com>
2020-10-27 10:51:09 +0000

    Add appinsights (#55)
    * Add appinsights
    
    * Fix test
    
    Co-authored-by: Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>

--- src/integrationTest/java/uk/gov/hmcts/dts/fact/config/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/dts/fact/config/SwaggerPublisherTest.java
@@ -1,11 +1,15 @@
 package uk.gov.hmcts.dts.fact.config;
 
+import com.microsoft.applicationinsights.web.internal.WebRequestTrackingFilter;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockFilterConfig;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -13,10 +17,11 @@ import java.nio.file.Paths;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 /**
- * Built-in feature which saves service's swagger specs in temporary directory. Each travis run on
- * master should automatically save and upload (if updated) documentation.
+ * Built-in feature which saves service's swagger specs in temporary directory. Each travis run on master should
+ * automatically save and upload (if updated) documentation.
  */
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -25,15 +30,25 @@ class SwaggerPublisherTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private WebApplicationContext wac;
+
+    @BeforeEach
+    void setUp() {
+        WebRequestTrackingFilter filter = new WebRequestTrackingFilter();
+        filter.init(new MockFilterConfig());
+        mvc = webAppContextSetup(wac).addFilters(filter).build();
+    }
+
     @DisplayName("Generate swagger documentation")
     @Test
     @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
     void generateDocs() throws Exception {
         byte[] specs = mvc.perform(get("/v2/api-docs"))
-                .andExpect(status().isOk())
-                .andReturn()
-                .getResponse()
-                .getContentAsByteArray();
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsByteArray();
 
         try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/swagger-specs.json"))) {
             outputStream.write(specs);
