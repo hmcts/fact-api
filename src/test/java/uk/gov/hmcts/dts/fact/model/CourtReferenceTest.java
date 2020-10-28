2e74a81b06542d33b208f8309ebbd9c7e56d2116
velimd <duzgunce.vd@gmail.com>
2020-10-28 15:16:18 +0000

    clean up

--- src/test/java/uk/gov/hmcts/dts/fact/model/CourtReferenceTest.java
+++ src/test/java/uk/gov/hmcts/dts/fact/model/CourtReferenceTest.java
@@ -4,21 +4,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import uk.gov.hmcts.dts.fact.entity.AddressType;
-import uk.gov.hmcts.dts.fact.entity.AreaOfLaw;
-import uk.gov.hmcts.dts.fact.entity.Contact;
 import uk.gov.hmcts.dts.fact.entity.Court;
-import uk.gov.hmcts.dts.fact.entity.CourtAddress;
-import uk.gov.hmcts.dts.fact.entity.CourtType;
-import uk.gov.hmcts.dts.fact.entity.Email;
-import uk.gov.hmcts.dts.fact.entity.Facility;
-import uk.gov.hmcts.dts.fact.entity.InPerson;
-import uk.gov.hmcts.dts.fact.entity.OpeningTime;
-import uk.gov.hmcts.dts.fact.entity.ServiceArea;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.util.Collections;
 import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -30,67 +19,9 @@ class CourtReferenceTest {
     @BeforeAll
     static void setUp() {
         courtEntity = new Court();
-
-        final ServiceArea serviceAreaEntity = new ServiceArea();
-        serviceAreaEntity.setService("Divorce");
-        courtEntity.setServiceArea(serviceAreaEntity);
-
-        final InPerson inPersonEntity = new InPerson();
-        inPersonEntity.setIsInPerson(true);
-        courtEntity.setInPerson(inPersonEntity);
-
-        final CourtAddress courtAddress = new CourtAddress();
-        courtAddress.setAddress("line 1\rline 2\nline3\r\nline4");
-        final AddressType addressType = new AddressType();
-        addressType.setName("Visit us or write to us");
-        courtAddress.setAddressType(addressType);
-        courtAddress.setPostcode("A post code");
-        courtAddress.setTownName("A town name");
-        courtEntity.setAddresses(Collections.singletonList(courtAddress));
-
-        final Email emailEntity = new Email();
-        emailEntity.setAddress("email address");
-        emailEntity.setDescription("email address description");
-        emailEntity.setExplanation("explanation for email address");
-        courtEntity.setEmails(Collections.singletonList(emailEntity));
-
-        final Contact contactEntity = new Contact();
-        contactEntity.setName("DX");
-        contactEntity.setNumber("123");
-        contactEntity.setExplanation("Explanation of contact");
-        contactEntity.setSortOrder(1);
-        courtEntity.setContacts(Collections.singletonList(contactEntity));
-
-        final AreaOfLaw areaOfLawEntity = new AreaOfLaw();
-        areaOfLawEntity.setName("Divorce");
-        areaOfLawEntity.setExternalLinkDescription("Description of url");
-        areaOfLawEntity.setExternalLink("http%3A//url");
-        courtEntity.setAreasOfLaw(Collections.singletonList(areaOfLawEntity));
-
-        final CourtType courtTypeEntity = new CourtType();
-        courtTypeEntity.setName("court type");
-        courtEntity.setCourtTypes(Collections.singletonList(courtTypeEntity));
-
-        final OpeningTime openingTimeEntity = new OpeningTime();
-        openingTimeEntity.setType("opening time type");
-        openingTimeEntity.setHours("opening times");
-        courtEntity.setOpeningTimes(Collections.singletonList(openingTimeEntity));
-
-        final Facility facilityEntity = new Facility();
-        facilityEntity.setDescription("<p>Description of facility</p>");
-        facilityEntity.setDescriptionCy("<p>Description of facility in Welsh</p>");
-        facilityEntity.setName("Facility");
-        courtEntity.setFacilities(Collections.singletonList(facilityEntity));
-
-        courtEntity.setInfo("<p>Info on court</p>");
-        courtEntity.setInfoCy("<p>Info on court in Welsh</p>");
         courtEntity.setName("Name");
         courtEntity.setSlug("name-slug");
         courtEntity.setNameCy("Name in Welsh");
-        courtEntity.setDirections("Directions");
-        courtEntity.setDirectionsCy("Directions in Welsh");
-        courtEntity.setAlert("Alert");
-        courtEntity.setAlertCy("Alert in Welsh");
         courtEntity.setUpdatedAt(currentTime);
     }
 
