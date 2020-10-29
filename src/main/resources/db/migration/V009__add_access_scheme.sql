cb413485ef38c4afaa1ff2ba142a53f3455f52b6
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-10-29 20:58:20 +0000

    FACT-102: Add constraint

--- src/main/resources/db/migration/V009__add_access_scheme.sql
+++ src/main/resources/db/migration/V009__add_access_scheme.sql
@@ -1,5 +1,6 @@
 ALTER TABLE public.search_inperson
-	ADD COLUMN access_scheme boolean NOT NULL DEFAULT false;
+	ADD COLUMN access_scheme boolean NOT NULL default false
+		CONSTRAINT search_inperson_check CHECK (NOT (is_in_person = false AND access_scheme = true));
 
 COPY public.search_inperson (id, is_in_person, court_id, access_scheme) FROM stdin;
 5	t	1479949	t
