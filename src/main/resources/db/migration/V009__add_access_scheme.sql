29aa4f5bda2f1c643833f8feebd80f12406b38f5
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-10-29 20:41:33 +0000

    FACT-102: Add data

--- src/main/resources/db/migration/V009__add_access_scheme.sql
+++ src/main/resources/db/migration/V009__add_access_scheme.sql
@@ -1,3 +1,8 @@
 ALTER TABLE public.search_inperson
 	ADD COLUMN access_scheme boolean NOT NULL DEFAULT false;
 
+COPY public.search_inperson (id, is_in_person, court_id, access_scheme) FROM stdin;
+5	t	1479949	t
+6	t	1479970	t
+7	t	1479729	f
+\.
