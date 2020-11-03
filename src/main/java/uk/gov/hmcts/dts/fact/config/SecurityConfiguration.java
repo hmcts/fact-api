f4345cb83921163a6b64a58862c75fd1fec6b6ee
velimd <duzgunce.vd@gmail.com>
2020-11-03 14:06:55 +0000

    undo SecurityConfiguration

--- src/main/java/uk/gov/hmcts/dts/fact/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/dts/fact/config/SecurityConfiguration.java
@@ -14,7 +14,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests(a -> a
-                .antMatchers(HttpMethod.GET, "/admin/courts").authenticated()
+                .antMatchers(HttpMethod.GET, "/courts/all").authenticated()
             )
             .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
