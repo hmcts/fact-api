1429d140a6d580eb88ce29179f985643574ac4b0
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-11-04 21:40:10 +0000

    Add forbidden tests

--- src/functionalTest/java/uk/gov/hmcts/dts/fact/AdminCourtsEndpointTest.java
+++ src/functionalTest/java/uk/gov/hmcts/dts/fact/AdminCourtsEndpointTest.java
@@ -13,7 +13,8 @@ import uk.gov.hmcts.dts.fact.model.admin.CourtGeneral;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.http.HttpHeaders.*;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 @ExtendWith({SpringExtension.class})
 @SpringBootTest(classes = {OAuthClient.class})
@@ -69,6 +70,20 @@ public class AdminCourtsEndpointTest {
     }
 
     @Test
+    public void shouldBeForbiddenForAllCourts() {
+        token = authClient.getNobodyToken();
+        final var response = given()
+            .relaxedHTTPSValidation()
+            .header(CONTENT_TYPE, CONTENT_TYPE_VALUE)
+            .header(AUTHORIZATION, BEARER + token)
+            .when()
+            .get("/courts/all")
+            .thenReturn();
+
+        assertThat(response.statusCode()).isEqualTo(403);
+    }
+
+    @Test
     public void shouldRetrieveCourtGeneralBySlug() {
         final var response = given()
             .relaxedHTTPSValidation()
@@ -81,6 +96,20 @@ public class AdminCourtsEndpointTest {
         assertThat(response.statusCode()).isEqualTo(200);
         final CourtGeneral court = response.as(CourtGeneral.class);
         assertThat(court.getName()).isEqualTo(BIRMINGHAM_CIVIL_AND_FAMILY_JUSTICE_CENTRE);
+    }
+
+    @Test
+    public void shouldBeForbiddenFromGettingCourtGeneralBySlug() {
+        token = authClient.getNobodyToken();
+        final var response = given()
+            .relaxedHTTPSValidation()
+            .header(CONTENT_TYPE, CONTENT_TYPE_VALUE)
+            .header(AUTHORIZATION, BEARER + token)
+            .when()
+            .get(COURT_DETAIL_BY_SLUG_ENDPOINT + BIRMINGHAM_CIVIL_AND_FAMILY_JUSTICE_CENTRE_SLUG + COURT_GENERAL_ENDPOINT)
+            .thenReturn();
+
+        assertThat(response.statusCode()).isEqualTo(403);
     }
 
     @Test
@@ -113,6 +142,34 @@ public class AdminCourtsEndpointTest {
         assertThat(court.getAlertCy()).isEqualTo(courtGeneral.getAlertCy());
         assertThat(court.getInfo()).isNotEqualTo(courtGeneral.getInfo());
         assertThat(court.getInfoCy()).isNotEqualTo(courtGeneral.getInfoCy());
+    }
+
+    @Test
+    public void shouldBeForbiddenToUpdateCourtGeneralBySlug() throws Exception {
+        CourtGeneral courtGeneral = new CourtGeneral(
+            BIRMINGHAM_CIVIL_AND_FAMILY_JUSTICE_CENTRE,
+            BIRMINGHAM_CIVIL_AND_FAMILY_JUSTICE_CENTRE,
+            "Admin Info",
+            "Welsh Admin Info",
+            true,
+            "Admin Alert",
+            "Welsh Admin Alert"
+        );
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(courtGeneral);
+
+        token = authClient.getNobodyToken();
+        final var response = given()
+            .relaxedHTTPSValidation()
+            .header(CONTENT_TYPE, CONTENT_TYPE_VALUE)
+            .header(AUTHORIZATION, BEARER + token)
+            .body(json)
+            .when()
+            .put(COURT_DETAIL_BY_SLUG_ENDPOINT + BIRMINGHAM_CIVIL_AND_FAMILY_JUSTICE_CENTRE_SLUG + COURT_GENERAL_ENDPOINT)
+            .thenReturn();
+
+        assertThat(response.statusCode()).isEqualTo(403);
     }
 
     @Test
