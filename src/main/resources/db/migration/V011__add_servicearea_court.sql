8e7e45895ce2bc7efa9a2bf679efbe06712daca4
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-11-04 12:42:29 +0000

    skeleton

---
+++ src/main/resources/db/migration/V011__add_servicearea_court.sql
@@ -0,0 +1,32 @@
+DROP TABLE public.search_courtservicearea;
+
+CREATE TABLE public.search_serviceareacourt
+(
+    id             integer PRIMARY KEY    NOT NULL,
+    servicearea_id integer                NOT NULL,
+    court_id       integer                NOT NULL,
+    catchment_type character varying(250) NOT NULL
+);
+
+CREATE SEQUENCE public.search_serviceareacourt_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE public.search_serviceareacourt_id_seq OWNED BY public.search_serviceareacourt.id;
+
+ALTER TABLE ONLY public.search_serviceareacourt
+    ALTER COLUMN id SET DEFAULT nextval('public.search_serviceareacourt'::regclass);
+
+
+ALTER TABLE ONLY public.search_serviceareacourt
+    ADD CONSTRAINT fk_search_court_id FOREIGN KEY (court_id) REFERENCES public.search_court (id);
+
+ALTER TABLE ONLY public.search_serviceareacourt
+    ADD CONSTRAINT fk_search_servicearea_id FOREIGN KEY (servicearea_id) REFERENCES public.search_servicearea (id);
+
+COPY public.search_serviceareacourt (id, servicearea_id, court_id, catchment_type) FROM stdin;
+1	1	1479943	national
+\.
