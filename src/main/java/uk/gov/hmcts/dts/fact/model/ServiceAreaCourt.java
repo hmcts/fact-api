71f61775ddb81e3fbed7cb37b3c50e207becd34f
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-11-06 13:47:24 +0000

    Add name and slug

--- src/main/java/uk/gov/hmcts/dts/fact/model/ServiceAreaCourt.java
+++ src/main/java/uk/gov/hmcts/dts/fact/model/ServiceAreaCourt.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.dts.fact.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import uk.gov.hmcts.dts.fact.entity.Court;
 
 
 @Data
@@ -9,9 +10,14 @@ import lombok.NoArgsConstructor;
 public class ServiceAreaCourt {
     private String slug;
     private String catchmentType;
+    private String courtName;
+    private String courtNameCy;
 
     public ServiceAreaCourt(uk.gov.hmcts.dts.fact.entity.ServiceAreaCourt serviceAreaCourt) {
-        this.slug = serviceAreaCourt.getCourt().getSlug();
         this.catchmentType = serviceAreaCourt.getCatchmentType();
+        final Court court = serviceAreaCourt.getCourt();
+        this.courtName = court.getName();
+        this.courtNameCy = court.getNameCy();
+        this.slug = court.getSlug();
     }
 }
