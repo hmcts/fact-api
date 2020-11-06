1abb41033358f997b9cc158c3378aef9d97fa909
Shaun Francis <shaun.francis1@hmcts.net>
2020-11-06 16:20:53 +0000

    Add slug to service area model

--- src/main/java/uk/gov/hmcts/dts/fact/model/ServiceArea.java
+++ src/main/java/uk/gov/hmcts/dts/fact/model/ServiceArea.java
@@ -14,11 +14,15 @@ import static uk.gov.hmcts.dts.fact.util.Utils.chooseString;
 public class ServiceArea {
     private String name;
     private String description;
+    private String slug;
+    private String serviceareaType;
     private List<ServiceAreaCourt> serviceAreaCourts;
 
     public ServiceArea(uk.gov.hmcts.dts.fact.entity.ServiceArea serviceArea) {
         this.name = chooseString(serviceArea.getNameCy(), serviceArea.getName());
         this.description = chooseString(serviceArea.getDescriptionCy(), serviceArea.getDescription());
+        this.slug = serviceArea.getSlug();
+        this.serviceareaType = serviceArea.getServiceareaType();
         this.serviceAreaCourts = serviceArea.getServiceAreaCourts() == null ? null : serviceArea.getServiceAreaCourts().stream().map(
             ServiceAreaCourt::new).collect(toList());
     }
