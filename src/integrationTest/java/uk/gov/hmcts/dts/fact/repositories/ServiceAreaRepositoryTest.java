cba4579e70bb4594b862bce7a0e7d566bc866fd0
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-11-08 11:02:18 +0000

    update test

--- src/integrationTest/java/uk/gov/hmcts/dts/fact/repositories/ServiceAreaRepositoryTest.java
+++ src/integrationTest/java/uk/gov/hmcts/dts/fact/repositories/ServiceAreaRepositoryTest.java
@@ -20,7 +20,7 @@ public class ServiceAreaRepositoryTest {
     private ServiceAreaRepository serviceAreaRepository;
 
     @Test
-    void shouldFindExistingServiceAreaWithNationalCatchmentTypeCourt() {
+    void shouldFindExistingServiceAreaWithLocalCatchmentTypeCourt() {
 
         final Optional<uk.gov.hmcts.dts.fact.entity.ServiceArea> result = serviceAreaRepository
             .findBySlugIgnoreCase("money-claims");
@@ -29,14 +29,15 @@ public class ServiceAreaRepositoryTest {
         final ServiceArea serviceArea = new ServiceArea(result.get());
         assertThat(serviceArea.getServiceAreaCourts().size()).isEqualTo(1);
         final List<ServiceAreaCourt> catchmentTypes = serviceArea.getServiceAreaCourts();
-        assertThat(catchmentTypes.get(0).getCatchmentType()).isEqualTo("national");
-        assertThat(catchmentTypes.get(0).getSlug()).isEqualTo("county-court-money-claims-centre-ccmcc");
-        assertThat(catchmentTypes.get(0).getCourtName()).isEqualTo("County Court Money Claims Centre (CCMCC)");
+        assertThat(catchmentTypes.get(0).getCatchmentType()).isEqualTo("local");
+        assertThat(catchmentTypes.get(0).getSlug()).isEqualTo("reading-county-court-and-family-court");
+        assertThat(catchmentTypes.get(0).getCourtName()).isEqualTo("Reading County Court and Family Court");
     }
 
     @Test
     void shouldNotFindNonExistentService() {
-        final Optional<uk.gov.hmcts.dts.fact.entity.ServiceArea> result = serviceAreaRepository.findBySlugIgnoreCase("nonExistent");
+        final Optional<uk.gov.hmcts.dts.fact.entity.ServiceArea> result = serviceAreaRepository.findBySlugIgnoreCase(
+            "nonExistent");
         assertThat(result).isEmpty();
     }
 
@@ -48,7 +49,8 @@ public class ServiceAreaRepositoryTest {
 
     @Test
     void shouldHandleNull() {
-        final Optional<uk.gov.hmcts.dts.fact.entity.ServiceArea> result = serviceAreaRepository.findBySlugIgnoreCase(null);
+        final Optional<uk.gov.hmcts.dts.fact.entity.ServiceArea> result = serviceAreaRepository.findBySlugIgnoreCase(
+            null);
         assertThat(result).isEmpty();
     }
 }
