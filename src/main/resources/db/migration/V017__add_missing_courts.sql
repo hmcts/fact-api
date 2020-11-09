7c2ba7549a469e9cb6ae014fa1b6afc67c560381
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-11-09 13:52:27 +0000

    add missing courts (#99)

---
+++ src/main/resources/db/migration/V017__add_missing_courts.sql
@@ -0,0 +1,12 @@
+COPY public.search_court (id, admin_id, name, slug, displayed, lat, lon, number, alert, directions, image_file,
+						  cci_code, updated_at, created_at, parking_id, info, hide_aols, defence_leaflet, info_leaflet,
+						  juror_leaflet, prosecution_leaflet, magistrate_code, welsh_enabled, alert_cy, directions_cy,
+						  info_cy, defence_leaflet_cy, info_leaflet_cy, prosecution_leaflet_cy, name_cy,
+						  gbs) FROM stdin;
+1480140	\N	Social Security and Child Support Appeals Service Centre	social-security-and-child-support-appeals-service-centre	t	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	f	\N	\N	\N	\N	\N	f	\N	\N	\N	\N	\N	\N	\N	\N
+1480142	\N	Bury St Edmunds Regional Divorce Centre	bury-st-edmunds-regional-divorce-centre	t	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	f	\N	\N	\N	\N	\N	f	\N	\N	\N	\N	\N	\N	\N	\N
+1480143	\N	Crime Service Centre	crime-service-centre	t	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	f	\N	\N	\N	\N	\N	f	\N	\N	\N	\N	\N	\N	\N	\N
+1480144	\N	Immigration and Asylum Appeals Service Centre	immigration-and-asylum-appeals-service-centre	t	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	f	\N	\N	\N	\N	\N	f	\N	\N	\N	\N	\N	\N	\N	\N
+1480145	\N	Money Claims Service Centre	money-claims-service-centre	t	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	f	\N	\N	\N	\N	\N	f	\N	\N	\N	\N	\N	\N	\N	\N
+1480146	\N	Probate Service Centre	probate-service-centre	t	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	f	\N	\N	\N	\N	\N	f	\N	\N	\N	\N	\N	\N	\N	\N
+\.
