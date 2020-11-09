cbf790a354d815a3e4790cb533e39f04765ed65a
Linus Norton <linusnorton@gmail.com>
2020-11-09 08:15:07 +0000

    Add logging for mapit calls

---
+++ src/main/java/uk/gov/hmcts/dts/fact/config/appinsights/DependencyProfiler.java
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.dts.fact.config.appinsights;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DependencyProfiler {
+
+    String name();
+
+    String action();
+}
+
