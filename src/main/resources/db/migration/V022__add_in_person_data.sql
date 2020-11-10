32b2232ee3c8e5a4041458f6d373008d0b2cb205
Linus Norton <linusnorton@gmail.com>
2020-11-10 13:30:35 +0000

    fix migration

--- src/main/resources/db/migration/V022__add_in_person_data.sql
+++ src/main/resources/db/migration/V022__add_in_person_data.sql
@@ -1,10 +1,10 @@
 
 COPY public.search_inperson (id, is_in_person, court_id, access_scheme) FROM stdin;
-9	f	1479943	national t
-10	f	1480140	national t
-11	f	1480143	national t
-12	f	1480144	national t
-13	f	1480146	national t
-14	f	1480147	national t
-15	f	1480148	national t
+9	f	1479943	f
+10	f	1480140	f
+11	f	1480143	f
+12	f	1480144	f
+13	f	1480146	f
+14	f	1480147	f
+15	f	1480148	f
 \.
