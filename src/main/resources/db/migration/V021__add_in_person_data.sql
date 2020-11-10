0a20d15ce28dfdd585f2223727d90fedf23f634d
Linus Norton <linusnorton@gmail.com>
2020-11-10 13:02:46 +0000

    Default to in-person court

---
+++ src/main/resources/db/migration/V021__add_in_person_data.sql
@@ -0,0 +1,10 @@
+
+COPY public.search_inperson (id, is_in_person, court_id, access_scheme) FROM stdin;
+9	f	1479943	national t
+10	f	1480140	national t
+11	f	1480143	national t
+12	f	1480144	national t
+13	f	1480146	national t
+14	f	1480147	national t
+15	f	1480148	national t
+\.
