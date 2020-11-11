c56ebbd62fbf3c1cc91264ab22775ca0ad1352cc
Shaun Francis <shaun.francis1@hmcts.net>
2020-11-11 16:21:19 +0000

    Add autowired logger and tests

--- src/main/java/uk/gov/hmcts/dts/fact/mapit/MapitFallbackClientFactory.java
+++ src/main/java/uk/gov/hmcts/dts/fact/mapit/MapitFallbackClientFactory.java
@@ -3,18 +3,20 @@ package uk.gov.hmcts.dts.fact.mapit;
 import feign.FeignException;
 import feign.hystrix.FallbackFactory;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 class MapitFallbackClientFactory implements FallbackFactory<MapitClient> {
+
+    @Autowired
+    private Logger logger;
 
     @Override
     public MapitClient create(final Throwable cause) {
 
         return new MapitClient() {
 
-            final Logger logger = LoggerFactory.getLogger(MapitClient.class);
             final String httpStatus = cause instanceof FeignException ? Integer.toString(((FeignException) cause).status()) : "";
             final String message = cause.getMessage();
 
