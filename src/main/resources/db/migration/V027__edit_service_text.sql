dbba28f8f55b4f6845c6b661455665d347ed7689
SarahLittlejohn <48680070+SarahLittlejohn@users.noreply.github.com>
2020-11-11 21:52:10 +0000

    Update text to include the rest of the dynamic sentence (#116)

---
+++ src/main/resources/db/migration/V027__edit_service_text.sql
@@ -0,0 +1,23 @@
+UPDATE public.search_servicearea
+SET text = 'We manage applications to dissolve a civil partnership at our central service centre. Find where to send your documents or ask about your application’s progress.'
+WHERE slug = 'civil-partnership';
+
+UPDATE public.search_servicearea
+SET text = 'We manage benefits appeals at our central service centre. Find where to send your documents or ask about your application’s progress.'
+WHERE slug = 'benefits';
+
+UPDATE public.search_servicearea
+SET text = 'We manage major criminal offences at our central service centre. Find where to send your documents or ask about your case.'
+WHERE slug = 'major-criminal-offences';
+
+UPDATE public.search_servicearea
+SET text = 'We manage your money claims applications at our central service centre. Find where to send your documents or ask about your application’s progress.'
+WHERE slug = 'money-claims';
+
+UPDATE public.search_servicearea
+SET text = 'We manage your probate applications at our central service centre. Find where to send your documents or ask about your applications progress.'
+WHERE slug = 'probate';
+
+UPDATE public.search_servicearea
+SET text = 'We manage minor criminal offences at our central service centre. Find where to send your documents or ask about your case.'
+WHERE slug = 'minor-criminal-offences';
