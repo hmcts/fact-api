c29bd29316e4d9c87225cc04cf63f447808d5ada
velimd <duzgunce.vd@gmail.com>
2020-11-12 15:53:18 +0000

    FACT 137 - include another controller for postcode search with required property only

---
+++ src/main/java/uk/gov/hmcts/dts/fact/model/CourtReferenceWithDistance.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.dts.fact.model;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import static uk.gov.hmcts.dts.fact.util.Utils.chooseString;
+
+@Getter
+@NoArgsConstructor
+@JsonPropertyOrder({"name", "slug", "distance"})
+public class CourtReferenceWithDistance {
+    private String name;
+    private String slug;
+    private BigDecimal distance;
+
+    public CourtReferenceWithDistance(uk.gov.hmcts.dts.fact.entity.CourtWithDistance courtEntity) {
+        this.name = chooseString(courtEntity.getNameCy(), courtEntity.getName());
+        this.slug = courtEntity.getSlug();
+        this.distance = BigDecimal.valueOf(courtEntity.getDistance()).setScale(2, RoundingMode.HALF_UP);
+    }
+}
