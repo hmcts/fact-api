3ccd1cb06d94b8ae97c8866802d66224069b55c6
Sarah Littlejohn <sarahjanelittlejohn@gmail.com>
2020-11-12 17:48:50 +0000

    Add more courts to service area

---
+++ src/main/resources/db/migration/V029__add_courts_to_servicearea.sql
@@ -0,0 +1,16 @@
+COPY public.search_serviceareacourt (servicearea_id, court_id, catchment_type) FROM stdin;
+1	1479373	local
+5	1479627	local
+17	1479444	local
+16	1479367	local
+8	1479627	local
+3	1479373	local
+14	1479373	local
+15	1479373	local
+13	1479373	local
+12	1479373	local
+6	1479429	local
+4	1479373	local
+11	1479373	local
+18	1479373	local
+\.
