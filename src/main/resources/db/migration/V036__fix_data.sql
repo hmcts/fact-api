f076e9f491f749b684030f46885cf247df29fe7e
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-11-19 22:57:03 +0000

    FACT_224: Prod data.

---
+++ src/main/resources/db/migration/V036__fix_data.sql
@@ -0,0 +1,33 @@
+drop table search_courtservicearea;
+
+UPDATE public.search_serviceareacourt
+SET court_id = 1479430
+WHERE id = 3;
+
+UPDATE public.search_serviceareacourt
+SET court_id = 1479430
+WHERE id = 4;
+
+UPDATE public.search_serviceareacourt
+SET court_id = '1480142'
+WHERE id = 5;
+
+UPDATE public.search_serviceareacourt
+SET court_id = 1480140
+WHERE id = 6;
+
+UPDATE public.search_serviceareacourt
+SET court_id = 1480136
+WHERE id = 7;
+
+UPDATE public.search_serviceareacourt
+SET court_id = 1480138
+WHERE id = 8;
+
+UPDATE public.search_serviceareacourt
+SET court_id = 1480142
+WHERE id = 9;
+
+UPDATE public.search_serviceareacourt
+SET court_id = 1480138
+WHERE id = 10;
