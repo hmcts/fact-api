2784bf844bbbcc2a897b8342dfccadd8309e2171
Sarah Littlejohn <sarahjanelittlejohn@gmail.com>
2020-11-24 16:22:31 +0000

    FACT-220: Clean up

--- src/main/java/uk/gov/hmcts/dts/fact/entity/AreaOfLaw.java
+++ src/main/java/uk/gov/hmcts/dts/fact/entity/AreaOfLaw.java
@@ -3,7 +3,10 @@ package uk.gov.hmcts.dts.fact.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "search_areaoflaw")
