9a766d0f5bfd4e941add61e1f98eb3c5ff6675a6
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-11-24 16:43:26 +0000

    FACT-255: correct servicearea

--- src/main/resources/db/migration/V043__fix_data.sql
+++ src/main/resources/db/migration/V043__fix_data.sql
@@ -1,5 +1,5 @@
 COPY public.search_serviceareacourt (servicearea_id, court_id, catchment_type) FROM stdin;
-2	1480143	national
+5	1480143	national
 \.
 
 COPY public.search_inperson (id, is_in_person, court_id, access_scheme) FROM stdin;
