337cbb3216d2a12e390a4b484e7436717c752791
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-11-24 16:47:12 +0000

    FACT-255: prod data (#163)
    * FACT-255: prod data
    
    * FACT-255: correct servicearea

---
+++ src/main/resources/db/migration/V043__fix_data.sql
@@ -0,0 +1,7 @@
+COPY public.search_serviceareacourt (servicearea_id, court_id, catchment_type) FROM stdin;
+5	1480143	national
+\.
+
+COPY public.search_inperson (id, is_in_person, court_id, access_scheme) FROM stdin;
+15	FALSE	1480143	FALSE
+\.
