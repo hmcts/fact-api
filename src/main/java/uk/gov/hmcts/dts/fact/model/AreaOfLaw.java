09cfbfd53669f3378a86d184c00c4bd28160d551
SarahLittlejohn <48680070+SarahLittlejohn@users.noreply.github.com>
2020-11-25 08:30:22 +0000

    Fact 220 aol display name (#165)
    * FACT-220: Tests
    
    * FACT-220: Implementation
    
    * FACT-220: Rename
    
    * FACT-220: Add welsh
    
    * FACT-220: Column added
    
    * FACT-220: Implementation
    
    * FACT-220: Tests
    
    * FACT-220: Rename
    
    * FACT-220: Welsh correction
    
    * FACT-220: Clean up
    
    * FACT-220: Rename
    
    * FACT-220: Add welsh translation

--- src/main/java/uk/gov/hmcts/dts/fact/model/AreaOfLaw.java
+++ src/main/java/uk/gov/hmcts/dts/fact/model/AreaOfLaw.java
@@ -16,11 +16,17 @@ public class AreaOfLaw {
     private String displayUrl;
     @JsonProperty("external_link_desc")
     private String externalLinkDescription;
+    @JsonProperty("display_name")
+    private String displayName;
+    @JsonProperty("display_external_link")
+    private String displayExternalLink;
 
     public AreaOfLaw(uk.gov.hmcts.dts.fact.entity.AreaOfLaw areaOfLaw) {
         this.name = areaOfLaw.getName();
         this.externalLink = chooseString(areaOfLaw.getExternalLinkCy(), areaOfLaw.getExternalLink());
         this.externalLinkDescription =
             chooseString(areaOfLaw.getExternalLinkDescriptionCy(), areaOfLaw.getExternalLinkDescription());
+        this.displayName = chooseString(areaOfLaw.getDisplayNameCy(), areaOfLaw.getDisplayName());
+        this.displayExternalLink = areaOfLaw.getDisplayExternalLink();
     }
 }
