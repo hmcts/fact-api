1e54c44bf903a61b3e4d25acddc4e2d1450a9c72
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-12-18 13:24:21 +0000

    FACT-271: Change major crime button name and hint text

---
+++ src/main/resources/db/migration/V052__change_servicearea.sql
@@ -0,0 +1,4 @@
+UPDATE public.search_servicearea
+SET description    = 'Single justice procedures, TV licence fines, speeding or parking fines.',
+	description_cy = 'Y Weithdrefn Un Ynad, Dirwyon trwydded deledu, dirwyon goryrru neu barcio.'
+WHERE description = 'TV licence fines, speeding or parking fines.';
