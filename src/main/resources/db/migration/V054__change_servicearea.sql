0b7e6a002b6e7f44b16117644092445790976710
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2021-01-07 11:12:02 +0000

    FACT-282: Add missing apostrophe

---
+++ src/main/resources/db/migration/V054__change_servicearea.sql
@@ -0,0 +1,3 @@
+UPDATE public.search_servicearea
+SET text = 'We manage your probate applications at our central service centre. Find where to send your documents or ask about your application''s progress.'
+WHERE slug = 'probate';
