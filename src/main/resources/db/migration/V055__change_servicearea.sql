0607802759489b15546c83d85c0bef7395190283
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2021-01-07 11:45:36 +0000

    FACT-269: Add missing full stop.

---
+++ src/main/resources/db/migration/V055__change_servicearea.sql
@@ -0,0 +1,4 @@
+UPDATE public.search_servicearea
+SET description    = 'Applying for probate, storing a will, settlement and disputes over a deceased person''s property, money and possessions.',
+	description_cy = 'Gwneud cais am brofiant, storio ewyllys, setliad ac anghydfod ynghylch eiddo, arian a meddiannau unigolyn ymadawedig.'
+WHERE slug = 'probate';
