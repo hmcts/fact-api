75d5016eca4567e3c3b41e0eb233e77cd793ae48
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2021-01-07 11:06:38 +0000

    FACT-273: Remove online url for civil partnership and immigration and asylum results (#194)

---
+++ src/main/resources/db/migration/V053__change_servicearea.sql
@@ -0,0 +1,11 @@
+UPDATE public.search_servicearea
+SET online_url     = NULL,
+	online_text    = NULL,
+	online_text_cy = NULL
+WHERE name = 'Civil partnership';
+
+UPDATE public.search_servicearea
+SET online_url     = NULL,
+	online_text    = NULL,
+	online_text_cy = NULL
+WHERE name = 'Immigration and asylum';
