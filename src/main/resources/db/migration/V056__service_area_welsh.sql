26b604a94644654166a76f5b76ad91bbcd1d4da4
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2021-01-13 11:58:18 +0000

    FACT-2967: Add welsh translation for new aol (#199)

---
+++ src/main/resources/db/migration/V056__service_area_welsh.sql
@@ -0,0 +1,11 @@
+UPDATE public.search_areaoflaw
+SET display_name_cy = 'Priodas dan orfod'
+WHERE name = 'Forced marriage';
+
+UPDATE public.search_areaoflaw
+SET display_name_cy = 'Anffurfio Organau Cenhedlu Benywod'
+WHERE name = 'FGM';
+
+UPDATE public.search_areaoflaw
+SET display_name_cy = 'Y Weithdrefn Un Ynad'
+WHERE name = 'Single justice procedure';
