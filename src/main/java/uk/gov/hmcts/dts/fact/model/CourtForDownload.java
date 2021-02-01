7c01a24ea470822a593a46c67bf221b7166aecc5
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2021-02-01 16:57:39 +0000

    FACT-166: Add aditional fields for download (#212)
    * FACT-166: Add aditional fields for download
    
    * FACT-166: format strings
    
    * FACT-166: suppress warning

--- src/main/java/uk/gov/hmcts/dts/fact/model/CourtForDownload.java
+++ src/main/java/uk/gov/hmcts/dts/fact/model/CourtForDownload.java
@@ -6,18 +6,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import uk.gov.hmcts.dts.fact.entity.AreaOfLaw;
-import uk.gov.hmcts.dts.fact.entity.CourtAddress;
+import uk.gov.hmcts.dts.fact.entity.CourtEmail;
+import uk.gov.hmcts.dts.fact.entity.CourtOpeningTime;
 import uk.gov.hmcts.dts.fact.entity.CourtType;
 import uk.gov.hmcts.dts.fact.entity.Facility;
 
 import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Locale;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
+import static java.lang.String.format;
+import static java.lang.System.lineSeparator;
 import static java.util.Optional.ofNullable;
+import static java.util.stream.Collectors.joining;
 
 @Getter
 @Setter
@@ -29,7 +32,7 @@ public class CourtForDownload {
     private String name;
     private String open;
     private String updated;
-    private String address;
+    private String addresses;
     private String areasOfLaw;
     private String courtTypes;
     private Integer crownCourtCode;
@@ -37,19 +40,20 @@ public class CourtForDownload {
     private Integer magistratesCourtCode;
     private String facilities;
     private String slug;
+    private String emails;
+    private String contacts;
+    private String openingTimes;
 
     public CourtForDownload(uk.gov.hmcts.dts.fact.entity.Court courtEntity) {
         this.name = courtEntity.getName();
         this.open = courtEntity.getDisplayed() ? "open" : "closed";
         this.updated = courtEntity.getUpdatedAt() == null
             ? null : new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).format(courtEntity.getUpdatedAt());
-
-        this.address = ofNullable(courtEntity.getAddresses())
+        this.addresses = ofNullable(courtEntity.getAddresses())
             .map(Collection::stream)
             .orElseGet(Stream::empty)
-            .findFirst()
             .map(this::formatAddress)
-            .orElse("");
+            .collect(joining(lineSeparator()));
         this.crownCourtCode = courtEntity.getNumber();
         this.countyCourtCode = courtEntity.getCciCode();
         this.magistratesCourtCode = courtEntity.getMagistrateCode();
@@ -57,29 +61,76 @@ public class CourtForDownload {
             .map(Collection::stream)
             .orElseGet(Stream::empty)
             .map(AreaOfLaw::getName)
-            .collect(Collectors.joining(", "));
+            .collect(joining(", "));
         this.courtTypes = ofNullable(courtEntity.getCourtTypes())
             .map(Collection::stream)
             .orElseGet(Stream::empty)
             .map(CourtType::getName)
-            .collect(Collectors.joining(", "));
+            .collect(joining(", "));
         this.facilities = ofNullable(courtEntity.getFacilities())
             .map(Collection::stream)
             .orElseGet(Stream::empty)
             .map(Facility::getName)
-            .collect(Collectors.joining(", "));
+            .collect(joining(", "));
         this.slug = courtEntity.getSlug();
+        this.emails = ofNullable(courtEntity.getCourtEmails())
+            .map(Collection::stream)
+            .orElseGet(Stream::empty)
+            .map(CourtEmail::getEmail)
+            .map(this::formatEmail)
+            .collect(joining(lineSeparator()));
+        this.contacts = ofNullable(courtEntity.getContacts())
+            .map(Collection::stream)
+            .orElseGet(Stream::empty)
+            .map(this::formatContact)
+            .collect(joining(lineSeparator()));
+        this.openingTimes = ofNullable(courtEntity.getCourtOpeningTimes())
+            .map(Collection::stream)
+            .orElseGet(Stream::empty)
+            .map(CourtOpeningTime::getOpeningTime)
+            .map(this::formatOpeningTime)
+            .collect(joining(lineSeparator()));
     }
 
-    private String formatAddress(CourtAddress courtAddress) {
-        return String.format(
-            "%s, %s, %s",
+    private String formatAddress(uk.gov.hmcts.dts.fact.entity.CourtAddress courtAddress) {
+        return format(
+            "%s: %s, %s, %s",
+            courtAddress.getAddressType().getName(),
             courtAddress.getAddress()
-                .replaceAll("\\n", ",")
-                .replaceAll("\\r", ",")
-                .replaceAll("\\t", ""),
+                .lines()
+                .filter(s -> !s.isBlank())
+                .map(s -> s.replaceAll("\t", ""))
+                .collect(joining(", ")),
             courtAddress.getTownName(),
             courtAddress.getPostcode()
         );
+    }
+
+    private String formatEmail(uk.gov.hmcts.dts.fact.entity.Email email) {
+        StringBuffer formatted = new StringBuffer(format(
+            "Address: %s, Description: %s",
+            email.getAddress(),
+            email.getDescription()
+        ));
+        if (email.getExplanation() != null && !email.getExplanation().isBlank()) {
+            formatted.append(format(", Explanation: %s", email.getExplanation()));
+        }
+        return formatted.toString();
+    }
+
+    private String formatContact(uk.gov.hmcts.dts.fact.entity.Contact contact) {
+        StringBuffer formatted = new StringBuffer(format(
+            "Number: %s, Description: %s",
+            contact.getNumber(),
+            contact.getName()
+        ));
+        if (contact.getExplanation() != null && !contact.getExplanation().isBlank()) {
+            formatted.append(format(", Explanation: %s", contact.getExplanation()));
+        }
+        return formatted.toString();
+    }
+
+    private String formatOpeningTime(uk.gov.hmcts.dts.fact.entity.OpeningTime openingTime) {
+        return format("Description: %s, Hours: %s", openingTime.getType(), openingTime.getHours());
     }
 }
--- src/test/java/uk/gov/hmcts/dts/fact/model/CourtForDownloadTest.java
+++ src/test/java/uk/gov/hmcts/dts/fact/model/CourtForDownloadTest.java
@@ -1,12 +1,19 @@
 package uk.gov.hmcts.dts.fact.model;
 
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.dts.fact.entity.AddressType;
 import uk.gov.hmcts.dts.fact.entity.AreaOfLaw;
+import uk.gov.hmcts.dts.fact.entity.Contact;
 import uk.gov.hmcts.dts.fact.entity.Court;
 import uk.gov.hmcts.dts.fact.entity.CourtAddress;
+import uk.gov.hmcts.dts.fact.entity.CourtEmail;
+import uk.gov.hmcts.dts.fact.entity.CourtOpeningTime;
 import uk.gov.hmcts.dts.fact.entity.CourtType;
+import uk.gov.hmcts.dts.fact.entity.Email;
 import uk.gov.hmcts.dts.fact.entity.Facility;
+import uk.gov.hmcts.dts.fact.entity.OpeningTime;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
@@ -16,6 +23,8 @@ import java.util.Locale;
 
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CourtForDownloadTest {
     static Court courtEntity;
@@ -32,6 +41,9 @@ class CourtForDownloadTest {
         courtAddress.setAddress("Address");
         courtAddress.setTownName("Town");
         courtAddress.setPostcode("Postcode");
+        AddressType addressType = new AddressType();
+        addressType.setName("Write to us");
+        courtAddress.setAddressType(addressType);
         List<CourtAddress> addresses = new ArrayList<>();
         addresses.add(courtAddress);
         courtEntity.setAddresses(addresses);
@@ -54,6 +66,24 @@ class CourtForDownloadTest {
         facilityTwo.setName("facility two");
         courtEntity.setFacilities(asList(facilityOne, facilityTwo));
         courtEntity.setSlug("name-slug");
+        Email email = new Email();
+        email.setAddress("email@address.com");
+        email.setDescription("Description of email address");
+        email.setExplanation("Explanation of email address");
+        CourtEmail courtEmail = mock(CourtEmail.class);
+        when(courtEmail.getEmail()).thenReturn(email);
+        courtEntity.setCourtEmails(asList(courtEmail));
+        Contact contact = new Contact();
+        contact.setName("Contact name");
+        contact.setNumber("12345");
+        contact.setExplanation("Contact explanation");
+        courtEntity.setContacts(asList(contact));
+        CourtOpeningTime courtOpeningTime = mock(CourtOpeningTime.class);
+        OpeningTime openingTime = new OpeningTime();
+        openingTime.setType("A description");
+        openingTime.setHours("Some opening hours");
+        when(courtOpeningTime.getOpeningTime()).thenReturn(openingTime);
+        courtEntity.setCourtOpeningTimes(asList(courtOpeningTime));
     }
 
     @Test
@@ -62,7 +92,7 @@ class CourtForDownloadTest {
         assertEquals("Name", court.getName());
         assertEquals("open", court.getOpen());
         assertEquals(new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).format(currentTime), court.getUpdated());
-        assertEquals("Address, Town, Postcode", court.getAddress());
+        assertEquals("Write to us: Address, Town, Postcode", court.getAddresses());
         assertEquals("Area of law one, Area of law two", court.getAreasOfLaw());
         assertEquals("court type one, court type two", court.getCourtTypes());
         assertEquals(111, court.getCrownCourtCode());
@@ -70,5 +100,11 @@ class CourtForDownloadTest {
         assertEquals(333, court.getMagistratesCourtCode());
         assertEquals("facility one, facility two", court.getFacilities());
         assertEquals(courtEntity.getSlug(), court.getSlug());
+        assertEquals(
+            "Address: email@address.com, Description: Description of email address, Explanation: Explanation of email address",
+            court.getEmails()
+        );
+        assertEquals("Description: A description, Hours: Some opening hours", court.getOpeningTimes());
+        assertEquals("Number: 12345, Description: Contact name, Explanation: Contact explanation", court.getContacts());
     }
 }
