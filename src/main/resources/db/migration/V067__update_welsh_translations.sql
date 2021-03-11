51173eedb08221efac50ed76dd88f388878d2070
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2021-03-11 11:59:43 +0000

    FACT-361 - Implemented welsh translations in new flyway transformation

---
+++ src/main/resources/db/migration/V067__update_welsh_translations.sql
@@ -0,0 +1,11 @@
+UPDATE public.search_service
+SET description_cy = 'Ceisio lloches, hawl i fyw yn y DU, ac apelio yn erbyn allgludiad.'
+WHERE id = 5;
+
+UPDATE public.search_service
+SET description_cy = 'Y Weithdrefn Un Ynad ac achosion troseddol eraill yn Llys y Goron neu''r Llys Ynadon.'
+WHERE id = 6;
+
+UPDATE public.search_servicearea
+SET description_cy = 'Ceisio lloches, hawl i fyw yn y DU, ac apelio yn erbyn allgludiad.'
+WHERE slug = 'immigration'
