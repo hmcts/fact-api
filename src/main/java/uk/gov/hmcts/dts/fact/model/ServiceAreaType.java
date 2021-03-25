620bea4d09a93cc484718cd0b37a1b6a4d307723
kiankwa <kian.kwa@justice.gov.uk>
2021-03-25 15:13:26 +0000

    Fixed issues with Scottish and Northern Irish postcode search results not filtered on deprecated search endpoint

--- src/main/java/uk/gov/hmcts/dts/fact/model/ServiceAreaType.java
+++ src/main/java/uk/gov/hmcts/dts/fact/model/ServiceAreaType.java
@@ -6,16 +6,10 @@ public enum ServiceAreaType {
     OTHER;
 
     public static ServiceAreaType serviceAreaTypeFrom(final String type) {
-        if (FAMILY.isEqualTo(type)) {
-            return FAMILY;
-        } else if (CIVIL.isEqualTo(type)) {
-            return CIVIL;
+        try {
+            return ServiceAreaType.valueOf(type.toUpperCase());
+        } catch (final IllegalArgumentException e) {
+            return OTHER;
         }
-
-        return OTHER;
-    }
-
-    public boolean isEqualTo(final String type) {
-        return this.toString().equalsIgnoreCase(type);
     }
 }
