d8c38257cd4fcbc326de2c5960fc1db05179bd2f
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2021-03-30 11:11:48 +0100

    FACT-384 - Added flyway transformation to set alert message for bradford & keighley magistrates court

---
+++ src/main/resources/db/migration/V074__update_bradford_magistrates_alert.sql
@@ -0,0 +1,3 @@
+UPDATE search_court
+SET alert = 'Bradford Magistrates Court building is currently closed. Those with listed cases should ring the enquiries number to get more details about their case.'
+WHERE slug = 'bradford-and-keighley-magistrates-court-and-family-court';
