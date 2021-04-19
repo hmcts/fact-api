44324d28eb1f3223d634e2c9ecad9a30ac0b2d6a
KianKwa <80688761+KianKwa@users.noreply.github.com>
2021-04-19 12:24:48 +0100

    FACT-408 - Add new admin endpoints for getting and updating court opening times (#270)
    * Add new admin endpoints for getting and updating sourt opening times
    
    * Fix PMD complaint
    
    * Added global exception handling

--- src/main/java/uk/gov/hmcts/dts/fact/controllers/ServiceAreasController.java
+++ src/main/java/uk/gov/hmcts/dts/fact/controllers/ServiceAreasController.java
@@ -2,16 +2,12 @@ package uk.gov.hmcts.dts.fact.controllers;
 
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.dts.fact.exception.NotFoundException;
 import uk.gov.hmcts.dts.fact.model.ServiceArea;
 import uk.gov.hmcts.dts.fact.services.ServiceAreaService;
 
@@ -34,11 +30,5 @@ public class ServiceAreasController {
     @ApiOperation("Return a service")
     public ResponseEntity<ServiceArea> getServiceArea(@PathVariable String slug) {
         return ok(serviceAreaService.getServiceArea(slug));
-    }
-
-    @ExceptionHandler(NotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    String slugNotFoundHandler(NotFoundException ex) {
-        return ex.getMessage();
     }
 }
---
+++ src/main/java/uk/gov/hmcts/dts/fact/services/admin/AdminCourtOpeningTimeService.java
@@ -0,0 +1,75 @@
+package uk.gov.hmcts.dts.fact.services.admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.dts.fact.entity.Court;
+import uk.gov.hmcts.dts.fact.entity.CourtOpeningTime;
+import uk.gov.hmcts.dts.fact.exception.NotFoundException;
+import uk.gov.hmcts.dts.fact.model.OpeningTime;
+import uk.gov.hmcts.dts.fact.repositories.CourtRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+public class AdminCourtOpeningTimeService {
+    private final CourtRepository courtRepository;
+
+    @Autowired
+    public AdminCourtOpeningTimeService(final CourtRepository courtRepository) {
+        this.courtRepository = courtRepository;
+    }
+
+    public List<OpeningTime> getCourtOpeningTimesBySlug(final String slug) {
+        return courtRepository.findBySlug(slug)
+            .map(c -> c.getCourtOpeningTimes()
+                .stream()
+                .map(CourtOpeningTime::getOpeningTime)
+                .map(OpeningTime::new)
+                .collect(toList()))
+            .orElseThrow(() -> new NotFoundException(slug));
+    }
+
+    public List<OpeningTime> updateCourtOpeningTimes(final String slug, final List<OpeningTime> openingTimes) {
+        final Court courtEntity = courtRepository.findBySlug(slug)
+            .orElseThrow(() -> new NotFoundException(slug));
+        return saveNewOpeningTimes(courtEntity, openingTimes);
+    }
+
+    private List<OpeningTime> saveNewOpeningTimes(final Court courtEntity, final List<OpeningTime> openingTimes) {
+        List<uk.gov.hmcts.dts.fact.entity.OpeningTime> openingTimesEntity = getNewOpeningTimesEntity(openingTimes);
+        List<CourtOpeningTime> courtOpeningTimesEntity = getNewCourtOpeningTimesEntity(courtEntity, openingTimesEntity);
+
+        if (courtEntity.getCourtOpeningTimes() == null) {
+            courtEntity.setCourtOpeningTimes(courtOpeningTimesEntity);
+        } else {
+            courtEntity.getCourtOpeningTimes().clear();
+            courtEntity.getCourtOpeningTimes().addAll(courtOpeningTimesEntity);
+        }
+        final Court courtWithUpdatedOpeningTime = courtRepository.save(courtEntity);
+
+        return courtWithUpdatedOpeningTime.getCourtOpeningTimes()
+            .stream()
+            .map(CourtOpeningTime::getOpeningTime)
+            .map(OpeningTime::new)
+            .collect(toList());
+    }
+
+    private List<uk.gov.hmcts.dts.fact.entity.OpeningTime> getNewOpeningTimesEntity(final List<OpeningTime> openingTimes) {
+        return openingTimes.stream()
+            .map(o -> new uk.gov.hmcts.dts.fact.entity.OpeningTime(o.getType(), o.getTypeCy(), o.getHours()))
+            .collect(toList());
+    }
+
+    @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
+    private List<CourtOpeningTime> getNewCourtOpeningTimesEntity(final Court court, final List<uk.gov.hmcts.dts.fact.entity.OpeningTime> openingTimes) {
+        final List<CourtOpeningTime> courtOpeningTimes = new ArrayList<>();
+        for (int i = 0; i < openingTimes.size(); i++) {
+            final CourtOpeningTime courtOpeningTime = new CourtOpeningTime(court, openingTimes.get(i), i);
+            courtOpeningTimes.add(courtOpeningTime);
+        }
+        return courtOpeningTimes;
+    }
+}
