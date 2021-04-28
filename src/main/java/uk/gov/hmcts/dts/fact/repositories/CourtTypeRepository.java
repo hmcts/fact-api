658bda864a10791470b457119b7487f657b2c0fd
Majid-justice <Majid.Najib@justice.gov.uk>
2021-04-28 10:46:20 +0100

    Addressing all Kian's comments under pull request

--- src/main/java/uk/gov/hmcts/dts/fact/repositories/CourtTypeRepository.java
+++ src/main/java/uk/gov/hmcts/dts/fact/repositories/CourtTypeRepository.java
@@ -3,9 +3,7 @@ package uk.gov.hmcts.dts.fact.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uk.gov.hmcts.dts.fact.entity.CourtType;
 
-import java.util.Optional;
 
 public interface CourtTypeRepository extends JpaRepository<CourtType, Integer> {
 
-    Optional<CourtType> findByName(String name);
 }
