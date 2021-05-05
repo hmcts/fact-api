9abcc55550ad08ba7417775806ca3cb83705764d
KianKwa <80688761+KianKwa@users.noreply.github.com>
2021-05-05 16:26:44 +0100

    Add in person court information to general info (#316)

--- src/test/java/uk/gov/hmcts/dts/fact/services/admin/AdminCourtGeneralInfoServiceTest.java
+++ src/test/java/uk/gov/hmcts/dts/fact/services/admin/AdminCourtGeneralInfoServiceTest.java
@@ -2,31 +2,40 @@ package uk.gov.hmcts.dts.fact.services.admin;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.dts.fact.config.security.RolesProvider;
 import uk.gov.hmcts.dts.fact.entity.Court;
+import uk.gov.hmcts.dts.fact.entity.InPerson;
 import uk.gov.hmcts.dts.fact.model.admin.CourtGeneralInfo;
 import uk.gov.hmcts.dts.fact.repositories.CourtRepository;
 
 import java.util.Collections;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.dts.fact.services.admin.AdminRole.FACT_ADMIN;
 
 @ExtendWith({SpringExtension.class, MockitoExtension.class})
+@MockitoSettings(strictness = Strictness.LENIENT)
 @ContextConfiguration(classes = AdminCourtGeneralInfoService.class)
 public class AdminCourtGeneralInfoServiceTest {
     private static final String COURT_SLUG = "some slug";
     private static final String COURT_ALERT = "English alert";
     private static final String COURT_ALERT_CY = "Welsh alert";
-    private static final CourtGeneralInfo COURT_GENERAL_INFO = new CourtGeneralInfo(false, null, null, null, COURT_ALERT, COURT_ALERT_CY);
+    private static final CourtGeneralInfo COURT_GENERAL_INFO = new CourtGeneralInfo(false, false, false, null, null, COURT_ALERT, COURT_ALERT_CY);
 
     @Autowired
     private AdminCourtGeneralInfoService adminService;
@@ -58,5 +67,39 @@ public class AdminCourtGeneralInfoServiceTest {
         when(courtRepository.save(court)).thenReturn(court);
 
         assertThat(adminService.updateCourtGeneralInfo(COURT_SLUG, COURT_GENERAL_INFO)).isEqualTo(COURT_GENERAL_INFO);
+    }
+
+    @Test
+    void shouldReturnExpectedInPersonAndAccessSchemeWithoutInPersonInfo() {
+        when(court.getInPerson()).thenReturn(null);
+        when(courtRepository.findBySlug(COURT_SLUG)).thenReturn(Optional.of(court));
+
+        final CourtGeneralInfo result = adminService.getCourtGeneralInfoBySlug(COURT_SLUG);
+        assertThat(result.getInPerson()).isEqualTo(false);
+        assertThat(result.getAccessScheme()).isEqualTo(false);
+    }
+
+    @SuppressWarnings("PMD.UnusedPrivateMethod")
+    private static Stream<Arguments> parametersForInPersonTest() {
+        return Stream.of(
+            Arguments.of(false, false, false),
+            Arguments.of(false, true, false),
+            Arguments.of(true, false, false),
+            Arguments.of(true, true, true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parametersForInPersonTest")
+    void shouldReturnExpectedInPersonAndAccessSchemeWithInPersonInfo(final Boolean isInPersonCourt, final Boolean isAccessScheme, final Boolean expectedAccessScheme) {
+        final InPerson inPerson = mock(InPerson.class);
+        when(court.getInPerson()).thenReturn(inPerson);
+        when(inPerson.getIsInPerson()).thenReturn(isInPersonCourt);
+        when(inPerson.getAccessScheme()).thenReturn(isAccessScheme);
+        when(courtRepository.findBySlug(COURT_SLUG)).thenReturn(Optional.of(court));
+
+        final CourtGeneralInfo result = adminService.getCourtGeneralInfoBySlug(COURT_SLUG);
+        assertThat(result.getInPerson()).isEqualTo(isInPersonCourt);
+        assertThat(result.getAccessScheme()).isEqualTo(expectedAccessScheme);
     }
 }
