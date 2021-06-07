9b2ece8874c10bb37a29c89f6997e190093a3955
KianKwa <80688761+KianKwa@users.noreply.github.com>
2021-06-07 17:12:13 +0100

    FACT-449 - Add API smoke test to check the Mapit service is accessible and quota limit hasn't been reached (#353)
    * Fix issue with email/contact/opening hour description not picking up the new description on the admin table
    
    * Add a smoke test to check the Mapit service is accessible and the quota limit hasn't been reached
    
    * Update readme file
    
    * Extend Spring extension in smoke test

--- src/main/java/uk/gov/hmcts/dts/fact/mapit/MapItService.java
+++ src/main/java/uk/gov/hmcts/dts/fact/mapit/MapItService.java
@@ -6,21 +6,36 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.dts.fact.exception.MapitUsageException;
+
+import java.io.IOException;
 
 @Component
 public class MapItService {
+    private static final String QUOTA = "quota";
+    private static final String LIMIT = "limit";
+    private static final String CURRENT = "current";
+
     @Value("${mapit.url}")
     private String mapitUrl;
 
     @Value("${mapit.endpoint.quota}")
     private String mapitQuotaPath;
 
-    public boolean isUp() {
-        final ResponseEntity<JsonNode> responseEntity = new RestTemplate().getForEntity(getMapitQuotaEndpoint(), JsonNode.class);
-        return responseEntity.getStatusCode().equals(HttpStatus.OK);
-    }
+    @Value("${mapit.key}")
+    private String mapitKey;
 
-    private String getMapitQuotaEndpoint() {
-        return mapitUrl + mapitQuotaPath;
+    public boolean isUp() throws IOException {
+        final String fullPath = mapitUrl + mapitQuotaPath + "?api_key=" + mapitKey;
+        final ResponseEntity<JsonNode> response = new RestTemplate().getForEntity(fullPath, JsonNode.class);
+
+        final JsonNode quota = response.getBody().get(QUOTA);
+        final int limit = quota.get(LIMIT).asInt();
+        // Mapit quota limit will be zero if a valid Mapit key has been configured. If no key is supplied and the
+        // limit hasn't been reached, throw an exception so the Mapit service can be marked as 'down' for health check
+        if (limit != 0 && limit <= quota.get(CURRENT).asInt()) {
+            throw new MapitUsageException();
+        }
+        return response.getStatusCode().equals(HttpStatus.OK);
     }
 }
