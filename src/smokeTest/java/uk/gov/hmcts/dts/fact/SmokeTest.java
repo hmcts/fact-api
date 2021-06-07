07ca50c3a80c89a0f3d5efd6e4f4f5bbb57fbaad
kiankwa <kian.kwa@justice.gov.uk>
2021-06-07 16:37:50 +0100

    Extend Spring extension in smoke test

--- src/smokeTest/java/uk/gov/hmcts/dts/fact/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/dts/fact/SmokeTest.java
@@ -3,15 +3,16 @@ package uk.gov.hmcts.dts.fact;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.HttpStatus.OK;
 
-@SpringBootTest()
+@ExtendWith(SpringExtension.class)
 public class SmokeTest {
     private static final String CONTENT_TYPE_VALUE = "application/json";
     private static final String HEALTH_ENDPOINT = "/health";
