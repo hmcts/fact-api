2dbf4dbf6fb38b34a91b4fd52f2cfcebe2394c76
kiankwa <kian.kwa@justice.gov.uk>
2021-06-18 15:41:07 +0100

    Added post and delete admin endpoints for postcodes

--- src/main/java/uk/gov/hmcts/dts/fact/controllers/admin/AdminCourtPostcodeController.java
+++ src/main/java/uk/gov/hmcts/dts/fact/controllers/admin/AdminCourtPostcodeController.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.dts.fact.controllers.admin;
 
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.*;
 import uk.gov.hmcts.dts.fact.config.security.Role;
 import uk.gov.hmcts.dts.fact.services.admin.AdminCourtPostcodeService;
 
+import java.net.URI;
 import java.util.List;
 
+import static org.springframework.http.ResponseEntity.created;
 import static org.springframework.http.ResponseEntity.ok;
 import static uk.gov.hmcts.dts.fact.services.admin.AdminRole.FACT_ADMIN;
 import static uk.gov.hmcts.dts.fact.services.admin.AdminRole.FACT_SUPER_ADMIN;
@@ -34,10 +37,19 @@ public class AdminCourtPostcodeController {
         return ok(adminService.getCourtPostcodesBySlug(slug));
     }
 
-    @PutMapping(path = "/{slug}/postcodes")
-    @ApiOperation("Update court postcodes")
-    @Role({FACT_SUPER_ADMIN})
-    public ResponseEntity<List<String>> updateCourtPostcodes(@PathVariable String slug, @RequestBody List<String> postcodes) {
-        return ok(adminService.updateCourtPostcodes(slug, postcodes));
+    @PostMapping(path = "/{slug}/postcodes")
+    @ApiOperation("Add new court postcodes")
+    @Role(FACT_SUPER_ADMIN)
+    public ResponseEntity<List<String>> addCourtPostcodes(@PathVariable String slug, @RequestBody List<String> postcodes) {
+        final List<String> postcodeCreated = adminService.addCourtPostcodes(slug, postcodes);
+        return created(URI.create(StringUtils.EMPTY)).body(postcodeCreated);
+    }
+
+    @DeleteMapping(path = "/{slug}/postcodes")
+    @ApiOperation("Delete court postcodes")
+    @Role(FACT_SUPER_ADMIN)
+    public ResponseEntity deleteCourtPostcodes(@PathVariable String slug, @RequestBody List<String> postcodes) {
+        adminService.deleteCourtPostcodes(slug, postcodes);
+        return ok().build();
     }
 }
--- src/test/java/uk/gov/hmcts/dts/fact/controllers/admin/AdminCourtPostcodeControllerTest.java
+++ src/test/java/uk/gov/hmcts/dts/fact/controllers/admin/AdminCourtPostcodeControllerTest.java
@@ -16,9 +16,8 @@ import uk.gov.hmcts.dts.fact.services.admin.AdminCourtPostcodeService;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,6 +27,7 @@ public class AdminCourtPostcodeControllerTest {
     private static final String BASE_PATH = "/admin/courts/";
     private static final String POSTCODE_PATH = "/postcodes";
     private static final String TEST_SLUG = "test-slug";
+    private static final String NOT_FOUND_PREFIX ="Not found: ";
     private static final List<String> TEST_POSTCODES = Arrays.asList("M11AA", "M11BB", "M11CC");
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
@@ -59,30 +59,53 @@ public class AdminCourtPostcodeControllerTest {
 
         mockMvc.perform(get(BASE_PATH + TEST_SLUG + POSTCODE_PATH))
             .andExpect(status().isNotFound())
-            .andExpect(content().string("Not found: " + TEST_SLUG));
+            .andExpect(content().string(NOT_FOUND_PREFIX + TEST_SLUG));
     }
 
     @Test
-    void updatePostcodesShouldReturnUpdatedCourtPostcodes() throws Exception {
-        when(adminService.updateCourtPostcodes(TEST_SLUG, TEST_POSTCODES)).thenReturn(TEST_POSTCODES);
+    void addPostcodesShouldReturnCreatedStatus() throws Exception {
+        when(adminService.addCourtPostcodes(TEST_SLUG, TEST_POSTCODES)).thenReturn(TEST_POSTCODES);
 
-        mockMvc.perform(put(BASE_PATH + TEST_SLUG + POSTCODE_PATH)
+        mockMvc.perform(post(BASE_PATH + TEST_SLUG + POSTCODE_PATH)
                             .content(expectedJson)
                             .contentType(MediaType.APPLICATION_JSON)
                             .accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
+            .andExpect(status().isCreated())
             .andExpect(content().json(expectedJson));
     }
 
     @Test
-    void updatePostcodesShouldReturnNotFoundForUnknownCourtSlug() throws Exception {
-        when(adminService.updateCourtPostcodes(TEST_SLUG, TEST_POSTCODES)).thenThrow(new NotFoundException(TEST_SLUG));
+    void addPostcodesShouldReturnNotFoundForUnknownCourtSlug() throws Exception {
+        doThrow(new NotFoundException(TEST_SLUG)).when(adminService).addCourtPostcodes(TEST_SLUG, TEST_POSTCODES);
 
-        mockMvc.perform(put(BASE_PATH + TEST_SLUG + POSTCODE_PATH)
+        mockMvc.perform(post(BASE_PATH + TEST_SLUG + POSTCODE_PATH)
                             .content(expectedJson)
                             .contentType(MediaType.APPLICATION_JSON)
                             .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isNotFound())
-            .andExpect(content().string("Not found: " + TEST_SLUG));
+            .andExpect(content().string(NOT_FOUND_PREFIX + TEST_SLUG));
+    }
+
+    @Test
+    void deletePostcodesShouldReturnOkStatus() throws Exception {
+       doNothing().when(adminService).deleteCourtPostcodes(TEST_SLUG, TEST_POSTCODES);
+
+        mockMvc.perform(delete(BASE_PATH + TEST_SLUG + POSTCODE_PATH)
+                            .content(expectedJson)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    void deletePostcodesShouldReturnNotFoundForUnknownCourtSlug() throws Exception {
+        doThrow(new NotFoundException(TEST_SLUG)).when(adminService).deleteCourtPostcodes(TEST_SLUG, TEST_POSTCODES);
+
+        mockMvc.perform(delete(BASE_PATH + TEST_SLUG + POSTCODE_PATH)
+                            .content(expectedJson)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound())
+            .andExpect(content().string(NOT_FOUND_PREFIX + TEST_SLUG));
     }
 }
