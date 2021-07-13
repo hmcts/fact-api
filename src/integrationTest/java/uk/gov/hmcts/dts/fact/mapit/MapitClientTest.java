6e84014b252f2a30b9babebf3bd24e5a1c69756f
agraham01 <81160496+agraham01@users.noreply.github.com>
2021-07-13 14:35:58 +0100

    FACT-188 Add back disabled annotation on MapitClientTest.

--- src/integrationTest/java/uk/gov/hmcts/dts/fact/mapit/MapitClientTest.java
+++ src/integrationTest/java/uk/gov/hmcts/dts/fact/mapit/MapitClientTest.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
-//@Disabled("Need to investigate why Mapit Key not getting picked up")
+@Disabled("Need to investigate why Mapit Key not getting picked up")
 class MapitClientTest {
 
     @Autowired
