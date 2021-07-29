a23f75c4039e7760c49a2526f81bfa3724c0b888
abottenberg <alexbottenberg@hotmail.com>
2021-07-29 10:32:34 +0100

    Adding Admin Areas of Law controller, service and repository. Adding put endoint to Court Areas of Law controller

--- src/test/java/uk/gov/hmcts/dts/fact/controllers/admin/AdminCourtAreasOfLawControllerTest.java
+++ src/test/java/uk/gov/hmcts/dts/fact/controllers/admin/AdminCourtAreasOfLawControllerTest.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.dts.fact.exception.NotFoundException;
 import uk.gov.hmcts.dts.fact.model.admin.AreaOfLaw;
+import uk.gov.hmcts.dts.fact.model.admin.Email;
 import uk.gov.hmcts.dts.fact.services.admin.AdminCourtAreasOfLawService;
 
 import java.util.List;
@@ -16,6 +18,7 @@ import java.util.List;
 import static java.util.Arrays.asList;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.hmcts.dts.fact.util.TestHelper.getResourceAsJson;
@@ -55,6 +58,21 @@ public class AdminCourtAreasOfLawControllerTest {
         mockMvc.perform(get(BASE_PATH + TEST_SLUG + CHILD_PATH))
             .andExpect(status().isNotFound())
             .andExpect(content().string("Not found: " + TEST_SLUG));
+    }
+
+    @Test
+    void updateCourtAreasOfLawShouldReturnUpdatedCourtAreasOfLaw() throws Exception {
+        final String expectedJson = getResourceAsJson(TEST_COURT_AREAS_OF_LAW_PATH);
+        final List<AreaOfLaw> areasOfLaw = asList(OBJECT_MAPPER.readValue(expectedJson, AreaOfLaw[].class));
+
+        when(adminService.updateAreasOfLawForCourt(TEST_SLUG, areasOfLaw)).thenReturn(areasOfLaw);
+
+        mockMvc.perform(put(BASE_PATH + TEST_SLUG + CHILD_PATH)
+                    .content(expectedJson)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().json(expectedJson));
     }
 
 }
