4ede3ecc7292a16ba1157dd545b1f172f179983d
Majid-justice <Majid.Najib@justice.gov.uk>
2021-07-30 13:21:26 +0100

    amended facilities endpoints to make consistent with other tabs

--- src/test/java/uk/gov/hmcts/dts/fact/services/admin/AdminCourtFacilityServiceTest.java
+++ src/test/java/uk/gov/hmcts/dts/fact/services/admin/AdminCourtFacilityServiceTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.dts.fact.entity.*;
-import uk.gov.hmcts.dts.fact.exception.DuplicatedListItemException;
 import uk.gov.hmcts.dts.fact.exception.NotFoundException;
 import uk.gov.hmcts.dts.fact.repositories.CourtRepository;
 import uk.gov.hmcts.dts.fact.repositories.FacilityTypeRepository;
@@ -34,20 +33,19 @@ public class AdminCourtFacilityServiceTest {
     private static final String FACILITY_TYPE_NAME = "name";
     private static final List<Facility> COURT_FACILITIES = new ArrayList<>();
     private static final String NOT_FOUND = "Not found: ";
-    private static final String DUPLICATE_ERROR_MESSAGE = "Facility already exists: Facility3";
     private static final String FACILITY_1 = "Facility1";
     private static final String FACILITY_2 = "Facility2";
     private static final String FACILITY_3 = "Facility3";
-    private static final String FACILITY_4 = "Facility4";
     private static final String DESCRIPTION_1 = "Description1";
     private static final String DESCRIPTION_CY_1 = "DescriptionCy1";
     private static final String DESCRIPTION_2 = "Description2";
     private static final String DESCRIPTION_CY_2 = "DescriptionCy2";
-    private static final String DESCRIPTION_3 = "Description3";
-    private static final String DESCRIPTION_CY_3 = "DescriptionCy3";
-    private static final String DESCRIPTION_4 = "Description4";
-    private static final String DESCRIPTION_CY_4 = "DescriptionCy4";
-    private static final uk.gov.hmcts.dts.fact.model.admin.Facility FACILITY = new uk.gov.hmcts.dts.fact.model.admin.Facility(FACILITY_4,DESCRIPTION_4,DESCRIPTION_CY_4);
+
+    final List<uk.gov.hmcts.dts.fact.model.admin.Facility> expectedCourtFacilities = Arrays.asList(
+        new uk.gov.hmcts.dts.fact.model.admin.Facility(FACILITY_1,DESCRIPTION_1,DESCRIPTION_CY_1),
+        new uk.gov.hmcts.dts.fact.model.admin.Facility(FACILITY_2,DESCRIPTION_2,DESCRIPTION_CY_2),
+        new uk.gov.hmcts.dts.fact.model.admin.Facility(FACILITY_3,"updated1","updated2"));
+
 
     @Autowired
     private AdminCourtFacilityService adminCourtFacilityService;
@@ -87,7 +85,6 @@ public class AdminCourtFacilityServiceTest {
         COURT_FACILITIES.add(facility1);
         COURT_FACILITIES.add(facility2);
         COURT_FACILITIES.add(facility3);
-
     }
 
     @AfterEach
@@ -117,106 +114,27 @@ public class AdminCourtFacilityServiceTest {
 
     @Test
     void shouldUpdateCourtFacility() {
-
-        final List<uk.gov.hmcts.dts.fact.model.admin.Facility> expectedCourtFacilities = Arrays.asList(
-            new uk.gov.hmcts.dts.fact.model.admin.Facility(FACILITY_1,DESCRIPTION_1,DESCRIPTION_CY_1),
-            new uk.gov.hmcts.dts.fact.model.admin.Facility(FACILITY_2,DESCRIPTION_2,DESCRIPTION_CY_2),
-            new uk.gov.hmcts.dts.fact.model.admin.Facility(FACILITY_3,"updated1","updated2"));
-
-        final uk.gov.hmcts.dts.fact.model.admin.Facility updatedFacility = new uk.gov.hmcts.dts.fact.model.admin.Facility("Facility3","updated1","updated2");
         when(court.getFacilities()).thenReturn(COURT_FACILITIES);
         when(courtRepository.findBySlug(COURT_SLUG)).thenReturn(Optional.of(court));
         when(courtRepository.save(court)).thenReturn(court);
         when(facilityTypeRepository.findByName(FACILITY_TYPE_NAME)).thenReturn(Optional.of(facilityType));
 
-        assertThat(adminCourtFacilityService.updateCourtFacility(COURT_SLUG,updatedFacility))
+        assertThat(adminCourtFacilityService.updateCourtFacility(COURT_SLUG,expectedCourtFacilities))
             .hasSize(FACILITY_COUNT)
             .containsExactlyElementsOf(expectedCourtFacilities);
     }
+
 
     @Test
     void shouldReturnNotFoundWhenUpdatingCourtFacilityForNonExistentCourt() {
 
         when(courtRepository.findBySlug(COURT_SLUG)).thenReturn(Optional.empty());
 
-        assertThatThrownBy(() -> adminCourtFacilityService.updateCourtFacility(COURT_SLUG,FACILITY))
+        assertThatThrownBy(() -> adminCourtFacilityService.updateCourtFacility(COURT_SLUG,expectedCourtFacilities))
             .isInstanceOf(NotFoundException.class)
             .hasMessage(NOT_FOUND + COURT_SLUG);
     }
 
-    @Test
-    void shouldAddNewCourtFacility() {
 
-        final List<uk.gov.hmcts.dts.fact.model.admin.Facility> expectedCourtFacilities = Arrays.asList(
-            new uk.gov.hmcts.dts.fact.model.admin.Facility(FACILITY_1,DESCRIPTION_1,DESCRIPTION_CY_1),
-            new uk.gov.hmcts.dts.fact.model.admin.Facility(FACILITY_2,DESCRIPTION_2,DESCRIPTION_CY_2),
-            new uk.gov.hmcts.dts.fact.model.admin.Facility(FACILITY_3,DESCRIPTION_3,DESCRIPTION_CY_3),
-            new uk.gov.hmcts.dts.fact.model.admin.Facility(FACILITY_4,DESCRIPTION_4,DESCRIPTION_CY_4));
-
-        when(court.getFacilities()).thenReturn(COURT_FACILITIES);
-        when(courtRepository.findBySlug(COURT_SLUG)).thenReturn(Optional.of(court));
-        when(courtRepository.save(court)).thenReturn(court);
-        when(facilityTypeRepository.findByName(FACILITY_TYPE_NAME)).thenReturn(Optional.of(facilityType));
-
-        assertThat(adminCourtFacilityService.addCourtFacility(COURT_SLUG,FACILITY))
-            .hasSize(4)
-            .containsExactlyElementsOf(expectedCourtFacilities);
-    }
-
-
-    @Test
-    void shouldDisplayMessageWhenFacilityAlreadyExists() {
-
-
-        when(court.getFacilities()).thenReturn(COURT_FACILITIES);
-        when(courtRepository.findBySlug(COURT_SLUG)).thenReturn(Optional.of(court));
-        when(courtRepository.save(court)).thenReturn(court);
-        when(facilityTypeRepository.findByName(FACILITY_TYPE_NAME)).thenReturn(Optional.of(facilityType));
-
-        assertThatThrownBy(() -> adminCourtFacilityService.addCourtFacility(COURT_SLUG, new uk.gov.hmcts.dts.fact.model.admin.Facility(FACILITY_3,DESCRIPTION_3,DESCRIPTION_CY_3)))
-            .isInstanceOf(DuplicatedListItemException.class)
-            .hasMessage(DUPLICATE_ERROR_MESSAGE);
-    }
-
-    @Test
-    void shouldReturnNotFoundWhenAddingCourtFacilityForNonExistentCourt() {
-
-
-        when(courtRepository.findBySlug(COURT_SLUG)).thenReturn(Optional.empty());
-
-        assertThatThrownBy(() -> adminCourtFacilityService.addCourtFacility(COURT_SLUG,FACILITY))
-            .isInstanceOf(NotFoundException.class)
-            .hasMessage(NOT_FOUND + COURT_SLUG);
-    }
-
-    @Test
-    void shouldDeleteCourtFacility() {
-
-        final List<uk.gov.hmcts.dts.fact.model.admin.Facility> expectedCourtFacilities = Arrays.asList(
-            new uk.gov.hmcts.dts.fact.model.admin.Facility(FACILITY_1,DESCRIPTION_1,DESCRIPTION_CY_1),
-            new uk.gov.hmcts.dts.fact.model.admin.Facility(FACILITY_2,DESCRIPTION_2,DESCRIPTION_CY_2));
-
-        final uk.gov.hmcts.dts.fact.model.admin.Facility deletedFacility = new uk.gov.hmcts.dts.fact.model.admin.Facility(FACILITY_3,DESCRIPTION_3,DESCRIPTION_CY_3);
-
-        when(court.getFacilities()).thenReturn(COURT_FACILITIES);
-        when(courtRepository.findBySlug(COURT_SLUG)).thenReturn(Optional.of(court));
-        when(courtRepository.save(court)).thenReturn(court);
-        when(facilityTypeRepository.findByName(FACILITY_TYPE_NAME)).thenReturn(Optional.of(facilityType));
-
-        assertThat(adminCourtFacilityService.deleteCourtFacility(COURT_SLUG,deletedFacility))
-            .hasSize(2)
-            .containsExactlyElementsOf(expectedCourtFacilities);
-    }
-
-    @Test
-    void shouldReturnNotFoundWhenDeletingCourtFacilityForNonExistentCourt() {
-
-
-        when(courtRepository.findBySlug(COURT_SLUG)).thenReturn(Optional.empty());
-
-        assertThatThrownBy(() -> adminCourtFacilityService.deleteCourtFacility(COURT_SLUG,FACILITY))
-            .isInstanceOf(NotFoundException.class)
-            .hasMessage(NOT_FOUND + COURT_SLUG);
-    }
 }
 
