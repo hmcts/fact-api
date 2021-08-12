0b2c0a520de55636df803ea4b31d15c5fbf6cd61
Thomas Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2021-08-12 13:56:00 +0100

    mapit-key-fix - Re-added removed import

--- src/test/java/uk/gov/hmcts/dts/fact/mapit/MapItHealthServiceTest.java
+++ src/test/java/uk/gov/hmcts/dts/fact/mapit/MapItHealthServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import java.io.IOException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
