80c79fc3604a791a76a814b243db3a4b5ca00a5d
kiankwa <kian.kwa@justice.gov.uk>
2021-08-16 11:37:32 +0100

    Add log to feign config

--- src/main/java/uk/gov/hmcts/dts/fact/config/FeignConfiguration.java
+++ src/main/java/uk/gov/hmcts/dts/fact/config/FeignConfiguration.java
@@ -18,8 +18,10 @@ public class FeignConfiguration {
     public RequestInterceptor requestInterceptor() {
 
         log.info("*******key is " + key);
+        System.out.println("*******key is " + key);
         if (StringUtils.isNotBlank(key)) {
             log.info("******mapit key present!!!!!********");
+            System.out.println("*******key is " + key);
         }
         return template -> template.header("X-Api-Key", key);
     }
