c66072babfd3753cfc5a33b3b7754fd0e13781e6
kiankwa <kian.kwa@justice.gov.uk>
2021-08-16 11:09:00 +0100

    Add log to feign config

--- src/main/java/uk/gov/hmcts/dts/fact/config/FeignConfiguration.java
+++ src/main/java/uk/gov/hmcts/dts/fact/config/FeignConfiguration.java
@@ -1,11 +1,14 @@
 package uk.gov.hmcts.dts.fact.config;
 
 import feign.RequestInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class FeignConfiguration {
 
     @Value("${mapit.key}")
@@ -14,6 +17,10 @@ public class FeignConfiguration {
     @Bean
     public RequestInterceptor requestInterceptor() {
 
+        log.info("*******key is " + key);
+        if (StringUtils.isNotBlank(key) && key.length() == 39) {
+            log.info("******mapit key present!!!!!********");
+        }
         return template -> template.header("X-Api-Key", key);
     }
 }
