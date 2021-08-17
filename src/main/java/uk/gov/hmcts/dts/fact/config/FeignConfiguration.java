9afb6b4714c6d9a1698043dd66da3cac91762b65
Thomas-Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2021-08-17 10:38:01 +0100

    Update FeignConfiguration.java

--- src/main/java/uk/gov/hmcts/dts/fact/config/FeignConfiguration.java
+++ src/main/java/uk/gov/hmcts/dts/fact/config/FeignConfiguration.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.dts.fact.config;
 
 import feign.RequestInterceptor;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
