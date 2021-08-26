52e057e6501af4463f9e1fea4cc447475e87b1de
kiankwa <kian.kwa@justice.gov.uk>
2021-08-26 14:05:24 +0100

    Migrate DX codes fom the contact table to a new table

---
+++ src/test/java/uk/gov/hmcts/dts/fact/model/deprecated/CourtWithDistanceTest.java
@@ -0,0 +1,68 @@
+package uk.gov.hmcts.dts.fact.model.deprecated;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.dts.fact.entity.AddressType;
+import uk.gov.hmcts.dts.fact.entity.AreaOfLaw;
+import uk.gov.hmcts.dts.fact.entity.Court;
+import uk.gov.hmcts.dts.fact.entity.CourtAddress;
+import uk.gov.hmcts.dts.fact.entity.CourtDxCode;
+import uk.gov.hmcts.dts.fact.entity.CourtType;
+import uk.gov.hmcts.dts.fact.entity.DxCode;
+
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CourtWithDistanceTest {
+    private static final String TEST_DX_CODE = "DX 99";
+
+    private static final Court COURT_ENTITY = new Court();
+    private static final uk.gov.hmcts.dts.fact.entity.CourtWithDistance COURT_WITH_DISTANCE_ENTITY = new uk.gov.hmcts.dts.fact.entity.CourtWithDistance();
+
+    @BeforeAll
+    static void setUp() {
+        final DxCode dxCode = new DxCode();
+        dxCode.setCode(TEST_DX_CODE);
+
+        final CourtDxCode courtDxCode = new CourtDxCode();
+        courtDxCode.setDxCode(dxCode);
+
+        final CourtType courtType = new CourtType();
+        courtType.setName("Crown court");
+
+        final AddressType addressType = new AddressType();
+        addressType.setName("Visit us");
+
+        final CourtAddress courtAddress = new CourtAddress();
+        courtAddress.setAddress("1 High Street");
+        courtAddress.setTownName("London");
+        courtAddress.setPostcode("SW1A 1AA");
+        courtAddress.setAddressType(addressType);
+
+        final AreaOfLaw areaOfLaw = new AreaOfLaw();
+        areaOfLaw.setName("Adoption");
+
+        COURT_ENTITY.setCourtDxCodes(singletonList(courtDxCode));
+        COURT_ENTITY.setCourtTypes(singletonList(courtType));
+        COURT_ENTITY.setAddresses(singletonList(courtAddress));
+        COURT_ENTITY.setAreasOfLaw(singletonList(areaOfLaw));
+
+        COURT_WITH_DISTANCE_ENTITY.setDxCodes(singletonList(dxCode));
+        COURT_WITH_DISTANCE_ENTITY.setCourtTypes(singletonList(courtType));
+        COURT_WITH_DISTANCE_ENTITY.setAddresses(singletonList(courtAddress));
+        COURT_WITH_DISTANCE_ENTITY.setAreasOfLaw(singletonList(areaOfLaw));
+        COURT_WITH_DISTANCE_ENTITY.setDistance(10D);
+    }
+
+    @Test
+    void testDxNumberDuringCreationWithCourtEntity() {
+        final CourtWithDistance courtWithDistance = new CourtWithDistance(COURT_ENTITY);
+        assertThat(courtWithDistance.getDxNumber()).isEqualTo(TEST_DX_CODE);
+    }
+
+    @Test
+    void testDxNumberDuringCreationWithCourtWithDistanceEntity() {
+        final CourtWithDistance courtWithDistance = new CourtWithDistance(COURT_WITH_DISTANCE_ENTITY);
+        assertThat(courtWithDistance.getDxNumber()).isEqualTo(TEST_DX_CODE);
+    }
+}
--- src/test/java/uk/gov/hmcts/dts/fact/util/UtilsTest.java
+++ src/test/java/uk/gov/hmcts/dts/fact/util/UtilsTest.java
@@ -2,13 +2,10 @@ package uk.gov.hmcts.dts.fact.util;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.context.i18n.LocaleContextHolder;
-import uk.gov.hmcts.dts.fact.entity.Contact;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static uk.gov.hmcts.dts.fact.util.Utils.*;
@@ -16,52 +13,7 @@ import static uk.gov.hmcts.dts.fact.util.Utils.*;
 class UtilsTest {
 
     protected static final String ENGLISH = "english";
-    private static final String EXPLANATION_OF_CONTACT = "Explanation of contact";
     protected static final String WELSH = "welsh";
-
-    @Test
-    void testDxExtractContacts() {
-        Contact contact1 = new Contact();
-        contact1.setDescription("DX");
-        contact1.setNumber("123");
-        contact1.setExplanation(EXPLANATION_OF_CONTACT);
-
-        Contact contact2 = new Contact();
-        contact2.setDescription("Name of contact");
-        contact2.setNumber("456");
-        contact2.setExplanation(EXPLANATION_OF_CONTACT);
-
-        List<Contact> contacts = new ArrayList<>();
-        contacts.add(contact1);
-        contacts.add(contact2);
-        List<String> dxNumbers = contacts.stream().filter(NAME_IS_DX).map(Contact::getNumber).collect(toList());
-
-        assertEquals("123", dxNumbers.get(0));
-        assertThat(dxNumbers.size()).isEqualTo(1);
-        assertEquals(contacts.get(0), contact1);
-        assertEquals(contacts.get(1), contact2);
-    }
-
-    @Test
-    void testRemoveDxContacts() {
-        Contact contact1 = new Contact();
-        contact1.setDescription("DX");
-        contact1.setNumber("123");
-        contact1.setExplanation(EXPLANATION_OF_CONTACT);
-
-        Contact contact2 = new Contact();
-        contact2.setDescription("Name of contact");
-        contact2.setNumber("456");
-        contact2.setExplanation(EXPLANATION_OF_CONTACT);
-
-        List<Contact> contacts = new ArrayList<>();
-        contacts.add(contact1);
-        contacts.add(contact2);
-
-        List<Contact> contactsWithoutDx = contacts.stream().filter(NAME_IS_NOT_DX).collect(toList());
-        assertEquals(contactsWithoutDx.get(0).getDescription(), contact2.getDescription());
-        assertThat(contactsWithoutDx.size()).isEqualTo(1);
-    }
 
     @Test
     void testHtmlFilter() {
