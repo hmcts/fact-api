9522a6d38e79c2be9663c11442db35e2b9de7b19
Natasha Alker <NatashaAlker@yahoo.co.uk>
2021-11-02 08:56:16 +0000

    Created migration to amend facilities dropdown options

---
+++ src/main/resources/db/migration/V135__amend_facilities_dropdown_options.sql
@@ -0,0 +1,3 @@
+UPDATE search_facility SET name = 'Loop Hearing' WHERE name LIKE 'Hearing%';
+UPDATE search_facilityfacilitytype SET facility_type_id = 36 WHERE facility_type_id = 56;
+DELETE FROM admin_facilitytype WHERE id = 56;
