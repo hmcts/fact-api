d96e8caece8e14a71770393fc3ff802f961636cd
Natasha Alker <NatashaAlker@yahoo.co.uk>
2021-11-02 16:52:45 +0000

    Improved facility type migration script

--- src/main/resources/db/migration/V135__amend_facilities_dropdown_options.sql
+++ src/main/resources/db/migration/V135__amend_facilities_dropdown_options.sql
@@ -1,3 +1,7 @@
-UPDATE search_facility SET name = 'Loop Hearing' WHERE name LIKE 'Hearing%';
-UPDATE search_facilityfacilitytype SET facility_type_id = 36 WHERE facility_type_id = 56;
-DELETE FROM admin_facilitytype WHERE id = 56;
+UPDATE search_facility SET name = 'Loop Hearing' WHERE name LIKE 'Hearing Loop%';
+
+UPDATE search_facilityfacilitytype SET facility_type_id =
+  (select facility_type_id from search_facilityfacilitytype sfft join search_facility sf on sf.id = sfft.facility_id where name = 'Loop Hearing' )
+  WHERE facility_type_id = (select facility_type_id from search_facilityfacilitytype sfft join search_facility sf on sf.id = sfft.facility_id where name LIKE 'Hearing Loop%');
+
+DELETE FROM admin_facilitytype WHERE name LIKE 'Hearing Loop%';
