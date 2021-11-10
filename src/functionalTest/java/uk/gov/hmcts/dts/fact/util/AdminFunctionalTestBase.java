504bdb45aecf30571b1ce6bc4a82440f789d1349
Majid-justice <81159704+Majid-justice@users.noreply.github.com>
2021-11-10 13:38:23 +0000

    fact api testing enhancements (#501)
    * fact api testing enhancements
    
    * adding CVE-2021-22096 to suppression list relating to springboot upgrade

--- src/functionalTest/java/uk/gov/hmcts/dts/fact/util/AdminFunctionalTestBase.java
+++ src/functionalTest/java/uk/gov/hmcts/dts/fact/util/AdminFunctionalTestBase.java
@@ -2,7 +2,10 @@ package uk.gov.hmcts.dts.fact.util;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.dts.fact.Application;
 
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class AdminFunctionalTestBase extends FunctionalTestBase {
 
     @Autowired
