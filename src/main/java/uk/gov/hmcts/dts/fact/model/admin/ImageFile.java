2a4b31198e7f331a4d7de9a520f595fdfc6085d5
Alex Bottenberg <63108247+abottenberg@users.noreply.github.com>
2021-11-10 18:10:58 +0000

    Fact 932 (#499)
    * FACT-932: Adding methods to get and update court image to court controller and service. Adding update court image query to court repository. Adding functional tests
    
    * Find and update court photo endpoint tests
    
    * FACT-932: refactoring unit tests and removing response container from AdminCourtController methods
    
    * Adding ImageFile interface to deal with null image_file values
    
    * Updating unit tests for updating court image
    
    * Removing redundant call to court repository in updateCourtImage method
    
    * refactoring unit tests
    
    * Fixing pipeline
    
    Co-authored-by: Ayisha-Sharjeel <ayisha.sharjeel@justice.gov.uk>
    Co-authored-by: amstevenson <addstevenson@hotmail.com>

---
+++ src/main/java/uk/gov/hmcts/dts/fact/model/admin/ImageFile.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.dts.fact.model.admin;
+
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class ImageFile {
+    private String imageName;
+}
