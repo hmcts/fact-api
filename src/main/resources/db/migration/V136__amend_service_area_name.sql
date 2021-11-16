6084bf95ad27bdf6ffe0fa13761daf2e8c29c60e
Natasha <natashaalker@yahoo.co.uk>
2021-11-16 14:02:38 +0000

    Changed service area name from 'fgm' to 'fgm protection orders' (#503)

---
+++ src/main/resources/db/migration/V136__amend_service_area_name.sql
@@ -0,0 +1,5 @@
+UPDATE search_servicearea SET name = 'Female Genital Mutilation Protection Orders'
+WHERE name = 'Female Genital Mutilation';
+
+UPDATE search_servicearea SET name_cy = 'Gorchymyn Amddiffyn rhag Anffurfio Organau Cenhedlu Benywod'
+WHERE name_cy = 'Anffurfio Organau Cenhedlu Benywod';
