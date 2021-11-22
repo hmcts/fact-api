f6dcff348f238d7ebd0b489a0496ca14d268b78c
amstevenson <addstevenson@hotmail.com>
2021-11-22 17:17:25 +0000

    pushing up changes for testing

--- src/main/java/uk/gov/hmcts/dts/fact/config/FeignConfiguration.java
+++ src/main/java/uk/gov/hmcts/dts/fact/config/FeignConfiguration.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.dts.fact.config;
 
 import feign.RequestInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class FeignConfiguration {
 
@@ -13,6 +15,7 @@ public class FeignConfiguration {
 
     @Bean
     public RequestInterceptor requestInterceptor() {
+        log.info("API Key is: " + key);
         return template -> template.header("X-Api-Key", key);
     }
 }
