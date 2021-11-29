107dcb6a4d02cc4e3104f38c893cbcadb920a716
amstevenson <addstevenson@hotmail.com>
2021-11-29 17:20:03 +0000

    removing key debugging log

--- src/main/java/uk/gov/hmcts/dts/fact/config/FeignConfiguration.java
+++ src/main/java/uk/gov/hmcts/dts/fact/config/FeignConfiguration.java
@@ -1,12 +1,10 @@
 package uk.gov.hmcts.dts.fact.config;
 
 import feign.RequestInterceptor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Slf4j
 @Configuration
 public class FeignConfiguration {
 
@@ -15,7 +13,6 @@ public class FeignConfiguration {
 
     @Bean
     public RequestInterceptor requestInterceptor() {
-        log.info("Api key is: " + key);
         return template -> template.header("X-Api-Key", key);
     }
 }
