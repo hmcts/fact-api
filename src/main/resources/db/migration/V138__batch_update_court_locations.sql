e6e964545409942825dd74d725dc4a19813d0a63
Natasha Alker <NatashaAlker@yahoo.co.uk>
2021-11-30 12:49:38 +0000

    Batch update of court latitude and longitudes

---
+++ src/main/resources/db/migration/V138__batch_update_court_locations.sql
@@ -0,0 +1,135 @@
+UPDATE search_courtaddress SET postcode = 'HR1 2HX' FROM search_court WHERE slug = 'hereford-crown-court';
+
+UPDATE search_court SET lat = 53.48048062143129, lon = -2.239108916299376 FROM search_courtaddress
+WHERE slug = 'agricultural-land-and-drainage-first-tier-tribunal-property-chamber'
+  AND search_courtaddress.postcode = 'M1 4AH';
+
+UPDATE search_court SET lat = 51.81079810063036, lon = -0.8081997334788457 FROM search_courtaddress
+WHERE slug = 'aylesbury-crown-court' AND search_courtaddress.postcode = 'HP21 7FT';
+
+UPDATE search_court SET lat = 52.4733702070942, lon = -1.9302922126318172 FROM search_courtaddress
+WHERE slug = 'birmingham-social-security-and-child-support-tribunal' AND search_courtaddress.postcode = 'B16 6FR';
+
+UPDATE search_court SET lat = 51.482104588153256, lon = -3.166483121946167 FROM search_courtaddress
+WHERE slug = 'bristol-district-probate-registry' AND search_courtaddress.postcode = 'CF24 0RZ';
+
+UPDATE search_court SET lat = 52.248580575678, lon = 0.7110124566458097 FROM search_courtaddress
+WHERE slug = 'bury-st-edmunds-regional-divorce-centre' AND search_courtaddress.postcode = 'IP33 1TR';
+
+UPDATE search_court SET lat = 51.48504222300505, lon = -3.1658523958012883 FROM search_courtaddress
+WHERE slug = 'cardiff-social-security-and-child-support-tribunal' AND search_courtaddress.postcode = 'CF24 0AB';
+
+UPDATE search_court SET lat = 54.52400558506852, lon = -1.5498492081075927 FROM search_courtaddress
+WHERE slug = 'care-standards-tribunal' AND search_courtaddress.postcode = 'DL1 1RU';
+
+UPDATE search_court SET lat = 51.51364537843727, lon = -0.11354608101087316 FROM search_courtaddress
+WHERE slug = 'central-london-county-court-bankruptcy' AND search_courtaddress.postcode = 'WC2A 2LL';
+
+UPDATE search_court SET lat = 53.32577561171765, lon = -2.69446626503196 FROM search_courtaddress
+WHERE slug = 'cheshire-and-merseyside-central-payment-and-enforcement-centre'
+  AND search_courtaddress.postcode = 'WA7 9DU';
+
+UPDATE search_court SET lat = 55.018204067305724, lon = -1.4903295506342475 FROM search_courtaddress
+WHERE slug = 'cleveland-durham-northumbria-and-north-yorkshire-central-enforcement-unit'
+  AND search_courtaddress.postcode = 'NE29 1DZ';
+
+UPDATE search_court SET lat = 52.23730179858217, lon = -0.8999654576044167 FROM search_courtaddress
+WHERE slug = 'county-court-business-centre-ccbc' AND search_courtaddress.postcode = 'NN1 2LH';
+
+UPDATE search_court SET lat = 53.481199679370064, lon = -2.2804307841310867 FROM search_courtaddress
+WHERE slug = 'county-court-money-claims-centre-ccmcc' AND search_courtaddress.postcode = 'M5 0BY';
+
+UPDATE search_court SET lat = 51.51576958071265, lon = -0.11031630830157654 FROM search_courtaddress
+WHERE slug = 'employment-appeal-tribunal-england-and-wales' AND search_courtaddress.postcode = 'EC4A 1NL';
+
+UPDATE search_court SET lat = 52.6129122008096, lon = -1.1949112282338836 FROM search_courtaddress
+WHERE slug = 'general-regulatory-chamber' AND search_courtaddress.postcode = 'LE1 8DJ';
+
+UPDATE search_court SET lat = 50.85609960524373, lon = -0.9828252598408124 FROM search_courtaddress
+WHERE slug = 'havant-justice-centre' AND search_courtaddress.postcode = 'PO9 2AL';
+
+UPDATE search_court SET lat = 52.056225571032066, lon = -2.7125220737311877 FROM search_courtaddress
+WHERE slug = 'hereford-crown-court' AND search_courtaddress.postcode = 'HR1 2HX';
+
+UPDATE search_court SET lat = 51.237695162423975, lon = 0.5636568957299919 FROM search_courtaddress
+WHERE slug = 'kent-surrey-and-sussex-enforcement-business-centre' AND search_courtaddress.postcode = 'ME14 9PG';
+
+UPDATE search_court SET lat = 51.51996348562011, lon = -0.1329892080878904 FROM search_courtaddress
+WHERE slug = 'land-registration' AND search_courtaddress.postcode = 'WC1E 7LR';
+
+UPDATE search_court SET lat = 53.407863922339054, lon = -2.9886450776071034 FROM search_courtaddress
+WHERE slug = 'liverpool-social-security-and-child-support-tribunal' AND search_courtaddress.postcode = 'L2 5UZ';
+
+UPDATE search_court SET lat = 51.52456509525468, lon = -0.11204246364224286 FROM search_courtaddress
+WHERE slug = 'london-collection-and-compliance-centre' AND search_courtaddress.postcode = 'WC1A 9QD';
+
+UPDATE search_court SET lat = 52.248580575678, lon = 0.7110124566458097 FROM search_courtaddress
+WHERE slug = 'maintenance-enforcement-business-centre-england' AND search_courtaddress.postcode = 'IP33 1TR';
+
+UPDATE search_court SET lat = 51.59023912638668, lon = -3.785616166969202 FROM search_courtaddress
+WHERE slug = 'maintenance-enforcement-business-centre-wales' AND search_courtaddress.postcode = 'SA13 1SB';
+
+UPDATE search_court SET lat = 51.51843600382902, lon = -0.11451564791946277 FROM search_courtaddress
+WHERE slug = 'maintenance-enforcement-centre-london' AND search_courtaddress.postcode = 'WC1V 6NP';
+
+UPDATE search_court SET lat = 52.6129122008096, lon = -1.1949112282338836 FROM search_courtaddress
+WHERE slug = 'mental-health-tribunal' AND search_courtaddress.postcode = 'LE1 8BN';
+
+UPDATE search_court SET lat = 52.76281892287217, lon = -1.2983774568929496 FROM search_courtaddress
+WHERE slug = 'midlands-and-wales-regional-fixed-penalty-office' AND search_courtaddress.postcode = 'LE11 9HB';
+
+UPDATE search_court SET lat = 54.99757292919431, lon = -1.4593904724636966 FROM search_courtaddress
+WHERE slug = 'newcastle-district-probate-registry' AND search_courtaddress.postcode = 'NE29 6AR';
+
+UPDATE search_court SET lat = 53.74356847051462, lon = -1.5992866723158008 FROM search_courtaddress
+WHERE slug = 'north-regional-fixed-penalty-office' AND search_courtaddress.postcode = 'LS27 1DN';
+
+UPDATE search_court SET lat = 55.018204067305724, lon = -1.4903295506342475 FROM search_courtaddress
+WHERE slug = 'north-yorkshire-magistrates-courts-central-finance-unit' AND search_courtaddress.postcode = 'NE29 1DZ';
+
+UPDATE search_court SET lat = 51.75023110462087, lon = -1.2673667768810715 FROM search_courtaddress
+WHERE slug = 'oxford-and-southern-oxfordshire-magistrates-court' AND search_courtaddress.postcode = 'OX1 1RZ';
+
+UPDATE search_court SET lat = 51.77173791025515, lon = 0.09393137927544327 FROM search_courtaddress
+WHERE slug = 'probate-service-centre' AND search_courtaddress.postcode = 'CM20 9QF';
+
+UPDATE search_court SET lat = 52.203767818883804, lon = 0.13199485734132432 FROM search_courtaddress
+WHERE slug = 'residential-property-eastern-region-first-tier-tribunal-property-chamber'
+  AND search_courtaddress.postcode = 'CB1 1BA';
+
+UPDATE search_court SET lat = 51.51996348562011, lon = -0.1329892080878904 FROM search_courtaddress
+WHERE slug = 'residential-property-london-region-first-tier-tribunal-property-chamber'
+  AND search_courtaddress.postcode = 'WC1E 7LR';
+
+UPDATE search_court SET lat = 52.47622407821297, lon = -1.898515825999671 FROM search_courtaddress
+WHERE slug = 'residential-property-midlands-region-first-tier-tribunal-property-chamber'
+  AND search_courtaddress.postcode = 'B5 4UU';
+
+UPDATE search_court SET lat = 53.48048062143129, lon = -2.239108916299376 FROM search_courtaddress
+WHERE slug = 'residential-property-northern-region-first-tier-tribunal-property-chamber'
+  AND search_courtaddress.postcode = 'M1 4AH';
+
+UPDATE search_court SET lat = 50.85609960524373, lon = -0.9828252598408124 FROM search_courtaddress
+WHERE slug = 'residential-property-southern-region-first-tier-tribunal-property-chamber'
+  AND search_courtaddress.postcode = 'PO9 2AL';
+
+UPDATE search_court SET lat = 53.96124883255377, lon = -2.0132541009230494 FROM search_courtaddress
+WHERE slug = 'skipton-magistrates-court' AND search_courtaddress.postcode = 'BD23 1RH';
+
+UPDATE search_court SET lat = 51.54372842180033, lon = 0.7097287040452791 FROM search_courtaddress
+WHERE slug = 'south-regional-fixed-penalty-office' AND search_courtaddress.postcode = 'SS2 6EU';
+
+UPDATE search_court SET lat = 50.91923996673761, lon = -1.4307895324271838 FROM search_courtaddress
+WHERE slug = 'south-west-regional-divorce-centre' AND search_courtaddress.postcode = 'SO15 9GG';
+
+UPDATE search_court SET lat = 52.4733702070942, lon = -1.9302922126318172 FROM search_courtaddress
+WHERE slug = 'tax-chamber-first-tier-tribunal' AND search_courtaddress.postcode = 'B16 6TZ';
+
+UPDATE search_court SET lat = 51.51576958071265, lon = -0.11031630830157654 FROM search_courtaddress
+WHERE slug = 'upper-tribunal-administrative-appeals-chamber' AND search_courtaddress.postcode = 'EC4A 1NL';
+
+UPDATE search_court SET lat = 51.51576958071265, lon = -0.11031630830157654 FROM search_courtaddress
+WHERE slug = 'upper-tribunal-tax-and-chancery-chamber' AND search_courtaddress.postcode = 'EC4A 1NL';
+
+UPDATE search_court SET lat = 53.68327912686083, lon = -1.506198900085291 FROM search_courtaddress
+WHERE slug = 'wakefield-civil-and-family-justice-centre' AND search_courtaddress.postcode = 'WF1 2QN';
