866cf9ef0652f9d48fe0ab2c6ef30e9dd4cc30dd
amstevenson <addstevenson@hotmail.com>
2021-12-03 11:55:00 +0000

    update size

--- src/main/java/uk/gov/hmcts/dts/fact/model/admin/NewCourt.java
+++ src/main/java/uk/gov/hmcts/dts/fact/model/admin/NewCourt.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.dts.fact.model.admin;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
