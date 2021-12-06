4457267d8fec1bb8ae5c89c233f709f644ff8811
Adam Stevenson <addstevenson@hotmail.com>
2021-12-06 15:50:05 +0000

    Fact 175 (#528)
    * updates
    
    * update to migration and endpoint
    
    * resolved issue around failing when save and added in duplicate court check
    
    * updating values
    
    * updating versions, checkstyles pmd etc
    
    * adding in utils for repo and slug
    
    * adding more unit tests
    
    * adding more unit tests
    
    * build gradle updates
    
    * adding in swags
    
    * update size
    
    * update size
    
    * Adding post request tests for Admin courts endpoint
    
    * pmd stuff
    
    Co-authored-by: Ayisha-Sharjeel <ayisha.sharjeel@justice.gov.uk>

---
+++ src/main/java/uk/gov/hmcts/dts/fact/model/admin/NewCourt.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.dts.fact.model.admin;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Validated
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class NewCourt {
+    @Size(min = 1, max = 200)
+    @Pattern(regexp = "^[a-zA-Z0-9-' ]*$",
+        message = "Court name is not valid, should only contain a combination of characters, "
+            + "numbers, apostrophes or hyphens")
+    private String newCourtName;
+}
---
+++ src/main/resources/db/migration/V139__add_admintype_entries_for_creating_court.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.admin_audittype(name)
+VALUES('Create new court');
