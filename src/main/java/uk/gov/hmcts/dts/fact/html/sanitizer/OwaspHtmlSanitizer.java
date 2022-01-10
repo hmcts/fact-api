38a31e1791fa6beeb762c87cd0aa0d52ecfa46d8
Alex Bottenberg <63108247+abottenberg@users.noreply.github.com>
2022-01-10 09:27:37 +0000

    Fact 980 (#551)
    * FACT-980: Increasing character limit to 300 to allow for <a> attributes, adding OWASP HTML sanitizer dependency to clean up court alerts
    
    * FACT-980: Adding unit tests and fixing style
    
    * FACT-980: Updating owasp-java-html-sanitizer to latest version

---
+++ src/main/java/uk/gov/hmcts/dts/fact/html/sanitizer/OwaspHtmlSanitizer.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.dts.fact.html.sanitizer;
+
+import org.owasp.html.HtmlPolicyBuilder;
+import org.owasp.html.PolicyFactory;
+
+public final class OwaspHtmlSanitizer {
+    private OwaspHtmlSanitizer() {
+
+    }
+
+    public static String sanitizeHtml(String untrustedHtml) {
+        PolicyFactory policy = new HtmlPolicyBuilder()
+            .allowElements("a", "strong", "em")
+            .allowUrlProtocols("https")
+            .allowAttributes("href").onElements("a")
+            .requireRelNofollowOnLinks()
+            .toFactory();
+        return policy.sanitize(untrustedHtml);
+    }
+}
