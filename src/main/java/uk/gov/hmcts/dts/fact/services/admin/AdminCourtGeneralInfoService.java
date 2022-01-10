e07ee613a37f026b09b7ba37778618177cea918b
Alex Bottenberg <63108247+abottenberg@users.noreply.github.com>
2022-01-10 09:27:37 +0000

    Fact 980 (#551)
    * FACT-980: Increasing character limit to 300 to allow for <a> attributes, adding OWASP HTML sanitizer dependency to clean up court alerts
    
    * FACT-980: Adding unit tests and fixing style
    
    * FACT-980: Updating owasp-java-html-sanitizer to latest version

--- src/main/java/uk/gov/hmcts/dts/fact/services/admin/AdminCourtGeneralInfoService.java
+++ src/main/java/uk/gov/hmcts/dts/fact/services/admin/AdminCourtGeneralInfoService.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.dts.fact.config.security.RolesProvider;
 import uk.gov.hmcts.dts.fact.entity.Court;
 import uk.gov.hmcts.dts.fact.exception.NotFoundException;
+import uk.gov.hmcts.dts.fact.html.sanitizer.OwaspHtmlSanitizer;
 import uk.gov.hmcts.dts.fact.model.admin.CourtGeneralInfo;
 import uk.gov.hmcts.dts.fact.repositories.CourtRepository;
 import uk.gov.hmcts.dts.fact.util.AuditType;
@@ -40,8 +41,8 @@ public class AdminCourtGeneralInfoService {
             .orElseThrow(() -> new NotFoundException(slug));
 
         final CourtGeneralInfo originalGeneralInfo = new CourtGeneralInfo(courtEntity);
-        courtEntity.setAlert(generalInfo.getAlert());
-        courtEntity.setAlertCy(generalInfo.getAlertCy());
+        courtEntity.setAlert(OwaspHtmlSanitizer.sanitizeHtml(generalInfo.getAlert()));
+        courtEntity.setAlertCy(OwaspHtmlSanitizer.sanitizeHtml(generalInfo.getAlertCy()));
 
         if (rolesProvider.getRoles().contains(FACT_SUPER_ADMIN)) {
             checkIfUpdatedCourtIsValid(courtEntity.getName(), generalInfo.getName());
---
+++ src/test/java/uk/gov/hmcts/dts/fact/html/sanitzer/OwaspHtmlSanitizer.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.dts.fact.html.sanitzer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.dts.fact.html.sanitizer.OwaspHtmlSanitizer.sanitizeHtml;
+
+
+public class OwaspHtmlSanitizer {
+    @Test
+    void shouldSanitizeHtml() {
+        assertThat(sanitizeHtml("<p>urgent notice</p>")).isEqualTo("urgent notice");
+        assertThat(sanitizeHtml("<p>urgent notice <a hrfe='#'>https://www.google.com</a></p>"))
+            .isEqualTo("urgent notice https://www.google.com");
+        assertThat(sanitizeHtml("<p>urgent notice <strong>bold</strong></p>"))
+            .isEqualTo("urgent notice <strong>bold</strong>");
+    }
+}
