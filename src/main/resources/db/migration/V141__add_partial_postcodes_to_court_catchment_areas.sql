07b1f29bc2aa9574f5c7e12ca8586d667ee587ff
Natasha Alker <NatashaAlker@yahoo.co.uk>
2022-01-17 11:24:24 +0000

    Adding partial postcodes using court slugs

--- src/main/resources/db/migration/V141__add_partial_postcodes_to_court_catchment_areas.sql
+++ src/main/resources/db/migration/V141__add_partial_postcodes_to_court_catchment_areas.sql
@@ -1,9 +1,9 @@
 INSERT INTO search_courtpostcode(postcode, court_id)
-VALUES ('GU147', 1479949),
-       ('GU146', 1479949),
-       ('GU111', 1479949),
-       ('GU126', 1479949),
-       ('GU125', 1479949),
-       ('RH140', 1479875),
-       ('RH201', 1479930),
-       ('GU280', 1479930);
+VALUES ('GU147', (select id from search_court where slug = 'aldershot-justice-centre')),
+       ('GU146', (select id from search_court where slug = 'aldershot-justice-centre')),
+       ('GU111', (select id from search_court where slug = 'aldershot-justice-centre')),
+       ('GU126', (select id from search_court where slug = 'aldershot-justice-centre')),
+       ('GU125', (select id from search_court where slug = 'aldershot-justice-centre')),
+       ('RH140', (select id from search_court where slug = 'horsham-county-court-and-family-court')),
+       ('RH201', (select id from search_court where slug = 'worthing-county-court-and-family-court')),
+       ('GU280', (select id from search_court where slug = 'worthing-county-court-and-family-court'));
