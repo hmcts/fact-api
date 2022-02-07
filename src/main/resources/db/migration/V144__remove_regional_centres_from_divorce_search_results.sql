29e355f3bf21c5e66556d88b61534a2640c5763a
Natasha Alker <NatashaAlker@yahoo.co.uk>
2022-02-07 15:14:22 +0000

    removing id reference

--- src/main/resources/db/migration/V144__remove_regional_centres_from_divorce_search_results.sql
+++ src/main/resources/db/migration/V144__remove_regional_centres_from_divorce_search_results.sql
@@ -1,2 +1,2 @@
 DELETE FROM search_serviceareacourt
-WHERE servicearea.id IN (select id from search_servicearea where name = 'Divorce') AND catchment_type = 'regional';
+WHERE servicearea_id = (select id from search_servicearea where name = 'Divorce') AND catchment_type = 'regional';
