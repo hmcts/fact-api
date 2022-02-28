8853100cb58b98ff92dc9b0fff502d5a43fe32bf
amstevenson <addstevenson@hotmail.com>
2022-02-28 12:52:23 +0000

    adding roles for getting service area list

--- src/main/java/uk/gov/hmcts/dts/fact/controllers/admin/AdminServiceAreaController.java
+++ src/main/java/uk/gov/hmcts/dts/fact/controllers/admin/AdminServiceAreaController.java
@@ -1,18 +1,24 @@
 package uk.gov.hmcts.dts.fact.controllers.admin;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.dts.fact.config.security.Role;
 import uk.gov.hmcts.dts.fact.model.ServiceArea;
+import uk.gov.hmcts.dts.fact.model.admin.CourtType;
 import uk.gov.hmcts.dts.fact.services.ServiceAreaService;
 
 import java.util.List;
 
 import static org.springframework.http.ResponseEntity.ok;
+import static uk.gov.hmcts.dts.fact.services.admin.AdminRole.FACT_ADMIN;
+import static uk.gov.hmcts.dts.fact.services.admin.AdminRole.FACT_SUPER_ADMIN;
 
 @RestController
 @RequestMapping(
@@ -30,6 +36,12 @@ public class AdminServiceAreaController {
 
     @GetMapping()
     @ApiOperation("Return all service areas")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successful", response = CourtType.class, responseContainer = "List"),
+        @ApiResponse(code = 401, message = "Unauthorized"),
+        @ApiResponse(code = 403, message = "Forbidden"),
+    })
+    @Role({FACT_ADMIN, FACT_SUPER_ADMIN})
     public ResponseEntity<List<ServiceArea>> getServiceArea() {
         return ok(serviceAreaService.getAllServiceAreas());
     }
