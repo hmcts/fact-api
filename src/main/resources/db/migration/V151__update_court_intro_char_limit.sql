d1354ff3abf6b623eeb1ab86da4e42a3863a732e
abottenberg <alexbottenberg@hotmail.com>
2022-04-27 15:39:24 +0100

    FACT-1153: Renaming new migration file to increase char limit for court intro

--- src/main/resources/db/migration/V151__update_court_intro_char_limit.sql
+++ src/main/resources/db/migration/V151__update_court_intro_char_limit.sql
@@ -1,3 +1,3 @@
 --- FACT-1153 ---
-ALTER TABLE search_servicecentre ALTER COLUMN intro_paragraph TYPE varchar(650);
-ALTER TABLE search_servicecentre ALTER COLUMN intro_paragraph_cy TYPE varchar(650);
+ALTER TABLE search_servicecentre ALTER COLUMN intro_paragraph TYPE varchar(450);
+ALTER TABLE search_servicecentre ALTER COLUMN intro_paragraph_cy TYPE varchar(450);
