ffd50ba78c6366290a4572499b45ceb55c638e0b
amstevenson <addstevenson@hotmail.com>
2022-08-16 16:59:20 +0100

    amending telford

--- src/main/resources/db/migration/V170__address_types_5.sql
+++ src/main/resources/db/migration/V170__address_types_5.sql
@@ -1,24 +1,24 @@
+-- telford-county-court-and-family-court
+DELETE FROM public.search_courtsecondaryaddresstype
+WHERE address_id = (
+      SELECT sca.id
+      FROM public.search_court AS sc
+      JOIN public.search_courtaddress AS sca
+      JOIN public.search_addresstype AS sat
+      ON sca.address_type_id = sat.id
+      ON sc.id = sca.court_id
+      WHERE sc.slug = 'telford-county-court-and-family-court'
+      AND sca.postcode = 'ST1 3BP'
+      AND sat.id = (
+        SELECT id
+        FROM public.search_addresstype
+        WHERE name = 'Write to us'
+      )
+);
+
 DO $$
 
 BEGIN
-
-  -- telford-county-court-and-family-court
-  DELETE FROM search_courtsecondaryaddresstype
-  WHERE address_id = (
-        SELECT sca.id
-        FROM public.search_court AS sc
-        JOIN public.search_courtaddress AS sca
-        JOIN public.search_addresstype AS sat
-        ON sca.address_type_id = sat.id
-        ON sc.id = sca.court_id
-        WHERE sc.slug = 'telford-county-court-and-family-court'
-        AND sca.postcode = 'ST1 3BP'
-        AND sat.id = (
-          SELECT id
-          FROM public.search_addresstype
-          WHERE name = 'Write to us'
-        )
-  );
 
   INSERT INTO public.search_courtsecondaryaddresstype (address_id, area_of_law_id, court_type_id)
   VALUES (
