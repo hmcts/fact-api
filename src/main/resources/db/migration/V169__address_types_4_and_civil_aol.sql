da892cef3c1dd23741477b7a8af704ff3cbb3177
amstevenson <addstevenson@hotmail.com>
2022-08-16 15:49:44 +0100

    adding civil to loughborough

--- src/main/resources/db/migration/V169__address_types_4_and_civil_aol.sql
+++ src/main/resources/db/migration/V169__address_types_4_and_civil_aol.sql
@@ -83,3 +83,45 @@ EXCEPTION WHEN OTHERS THEN
   raise notice '% %', SQLERRM, SQLSTATE;
 END;
 $$;
+
+-- loughborough
+DO $$
+
+BEGIN
+  INSERT INTO public.search_courtsecondaryaddresstype (address_id, area_of_law_id, court_type_id)
+  VALUES (
+  (
+    SELECT id FROM public.search_courtaddress
+    WHERE id = (
+                                      SELECT sca.id
+      FROM public.search_court AS sc
+      JOIN public.search_courtaddress AS sca
+      JOIN public.search_addresstype AS sat
+      ON sca.address_type_id = sat.id
+      ON sc.id = sca.court_id
+      WHERE sc.slug = 'loughborough-court'
+      AND sca.postcode = 'LE1 6HG'
+      AND sat.id = (
+        SELECT id
+        FROM public.search_addresstype
+        WHERE name = 'Write to us'
+      )
+    )
+    AND address_type_id = (
+      SELECT id
+      FROM public.search_addresstype
+      WHERE name = 'Write to us'
+    )
+  ),
+  (
+    SELECT id
+    FROM public.search_areaoflaw
+    WHERE name = 'Civil'
+  ),
+  null);
+
+EXCEPTION WHEN OTHERS THEN
+  raise notice 'The row has not been added, as the address is not present';
+  raise notice '% %', SQLERRM, SQLSTATE;
+END;
+$$;
