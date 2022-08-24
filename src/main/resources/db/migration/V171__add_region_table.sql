701e7076e1476ff1798abfc4ac51b8fe8e6a0607
Natasha Alker <NatashaAlker@yahoo.co.uk>
2022-08-24 15:27:59 +0100

    add regions table

--- src/main/resources/db/migration/V171__add_region_table.sql
+++ src/main/resources/db/migration/V171__add_region_table.sql
@@ -17,19 +17,20 @@ ALTER SEQUENCE public.search_region_id_seq OWNED BY public.search_region.id;
 ALTER TABLE ONLY public.search_region
 ALTER COLUMN id SET DEFAULT nextval('public.search_region_id_seq'::regclass);
 
-COPY public.search_region (name, country) FROM stdin;
-Eastern England
-North West England
-North East England
-South East England
-South West England
-East Midlands England
-London England
-West Midlands England
-Yorkshire and the Humber England
-South Wales West Wales
-South Wales Central Wales
-South Wales East Wales
-Mid and West Wales Wales
-North Wales Wales
-\.
+INSERT INTO public.search_region (name, country)
+VALUES
+('Eastern', 'England'),
+('North West', 'England'),
+('North East', 'England'),
+('South East', 'England'),
+('South West', 'England'),
+('East Midlands', 'England'),
+('London', 'England'),
+('West Midlands', 'England'),
+('Yorkshire and the Humber', 'England'),
+('South Wales West', 'Wales'),
+('South Wales Central', 'Wales'),
+('South Wales East', 'Wales'),
+('Mid and West Wales', 'Wales'),
+('North Wales', 'Wales');
+
