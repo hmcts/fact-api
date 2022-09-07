6b1ddec8a5465e744e6bf90bcaf08072f99a4909
amstevenson <addstevenson@hotmail.com>
2022-09-07 11:54:42 +0100

    updating sql

--- src/main/resources/db/migration/V171__synchronise_contact_email_types.sql
+++ src/main/resources/db/migration/V171__synchronise_contact_email_types.sql
@@ -1,6 +1,6 @@
 INSERT INTO public.admin_contacttype (name, name_cy)
 SELECT et.description, et.description_cy FROM public.admin_contacttype ct -- ones in contact
-RIGHT JOIN public.admin_emailtype et -- ones in email
+RIGHT JOIN public.admin_emailtype et
 ON et.description = ct.name
 WHERE et.description NOT IN (
     SELECT ct.name
@@ -9,8 +9,8 @@ WHERE et.description NOT IN (
 
 INSERT INTO public.admin_emailtype (description, description_cy)
 SELECT ct.name, ct.name_cy
-FROM public.admin_emailtype et -- ones in contact
-         RIGHT JOIN public.admin_contacttype ct -- ones in email
+FROM public.admin_emailtype et
+         RIGHT JOIN public.admin_contacttype ct
                     on et.description = ct.name
 WHERE ct.name NOT IN (
     SELECT et.description
