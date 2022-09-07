2db110e2cfebb34b03d64dcac9a32437d9b5c22f
amstevenson <addstevenson@hotmail.com>
2022-09-07 11:43:07 +0100

    adding migration to sync two tables together

---
+++ src/main/resources/db/migration/V171__synchronise_contact_email_types.sql
@@ -0,0 +1,18 @@
+INSERT INTO public.admin_contacttype (name, name_cy)
+SELECT et.description, et.description_cy FROM public.admin_contacttype ct -- ones in contact
+RIGHT JOIN public.admin_emailtype et -- ones in email
+ON et.description = ct.name
+WHERE et.description NOT IN (
+    SELECT ct.name
+    FROM public.admin_contacttype ct
+);
+
+INSERT INTO public.admin_emailtype (description, description_cy)
+SELECT ct.name, ct.name_cy
+FROM public.admin_emailtype et -- ones in contact
+         RIGHT JOIN public.admin_contacttype ct -- ones in email
+                    on et.description = ct.name
+WHERE ct.name NOT IN (
+    SELECT et.description
+    FROM public.admin_emailtype et
+);
