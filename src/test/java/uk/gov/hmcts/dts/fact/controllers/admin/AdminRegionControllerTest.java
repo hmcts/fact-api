e7dc4433b3924ad6da6b6c323aa2ad25be089b51
Natasha Alker <NatashaAlker@yahoo.co.uk>
2022-09-13 17:54:43 +0100

    Add region controller and service tests

---
+++ src/test/java/uk/gov/hmcts/dts/fact/controllers/admin/AdminRegionControllerTest.java
@@ -0,0 +1,49 @@
+package uk.gov.hmcts.dts.fact.controllers.admin;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.dts.fact.model.admin.Region;
+import uk.gov.hmcts.dts.fact.services.admin.AdminRegionService;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AdminRegionController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class AdminRegionControllerTest {
+    private static final String PATH = "/admin/regions";
+    private static final List<Region> EXPECTED_REGIONS = Arrays.asList(
+        new Region(1, "North West", "England"),
+        new Region(2, "East Midlands", "Scotland"),
+        new Region(3,"Mid and West Wales","Wales")
+    );
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    @Autowired
+    private transient MockMvc mockMvc;
+
+    @MockBean
+    private AdminRegionService adminService;
+
+    @Test
+    void shouldRetrieveAllRegions() throws Exception {
+        when(adminService.getAllRegions()).thenReturn(EXPECTED_REGIONS);
+        final String regionsJson  = OBJECT_MAPPER.writeValueAsString(EXPECTED_REGIONS);
+
+        mockMvc.perform(get(PATH))
+            .andExpect(status().isOk())
+            .andExpect(content().json(regionsJson));
+
+    }
+
+}
---
+++ src/test/java/uk/gov/hmcts/dts/fact/services/admin/AdminRegionServiceTest.java
@@ -0,0 +1,76 @@
+package uk.gov.hmcts.dts.fact.services.admin;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.dts.fact.repositories.RegionRepository;
+
+import java.util.*;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith({SpringExtension.class, MockitoExtension.class})
+@ContextConfiguration(classes = AdminRegionService.class)
+@SuppressWarnings("PMD.TooManyMethods")
+public class AdminRegionServiceTest {
+    private static final int REGION_ID1 = 1;
+    private static final int REGION_ID2 = 2;
+    private static final int REGION_ID3 = 3;
+    private static final String REGION_NAME1 = "Region 1";
+    private static final String REGION_NAME2 = "Region 2";
+    private static final String REGION_NAME3 = "Region 3";
+    private static final String REGION_COUNTRY1 = "England";
+    private static final String REGION_COUNTRY2 = "Wales";
+    private static final String REGION_COUNTRY3 = "Scotland";
+    private static final uk.gov.hmcts.dts.fact.entity.Region REGION1 = new uk.gov.hmcts.dts.fact.entity.Region(REGION_ID1, REGION_NAME1, REGION_COUNTRY1);
+    private static final uk.gov.hmcts.dts.fact.entity.Region REGION2 = new uk.gov.hmcts.dts.fact.entity.Region(REGION_ID2, REGION_NAME2, REGION_COUNTRY2);
+    private static final uk.gov.hmcts.dts.fact.entity.Region REGION3 = new uk.gov.hmcts.dts.fact.entity.Region(REGION_ID3, REGION_NAME3, REGION_COUNTRY3);
+    private static final List<uk.gov.hmcts.dts.fact.entity.Region> REGIONS = Arrays.asList(REGION1, REGION2, REGION3);
+
+    @Autowired
+    private AdminRegionService adminRegionService;
+
+    @MockBean
+    private RegionRepository regionRepository;
+
+    @Test
+    void shouldReturnAllRegions() {
+        when(regionRepository.findAll()).thenReturn(REGIONS);
+        final List<uk.gov.hmcts.dts.fact.model.admin.Region> results = adminRegionService.getAllRegions();
+
+        final SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(results).hasSize(REGIONS.size());
+
+        softly.assertThat(results.get(0).getId()).isEqualTo(REGION_ID1);
+        softly.assertThat(results.get(0).getName()).isEqualTo(REGION_NAME1);
+        softly.assertThat(results.get(0).getCountry()).isEqualTo(REGION_COUNTRY1);
+
+        softly.assertThat(results.get(1).getId()).isEqualTo(REGION_ID2);
+        softly.assertThat(results.get(1).getName()).isEqualTo(REGION_NAME2);
+        softly.assertThat(results.get(1).getCountry()).isEqualTo(REGION_COUNTRY2);
+
+        softly.assertThat(results.get(2).getId()).isEqualTo(REGION_ID3);
+        softly.assertThat(results.get(2).getName()).isEqualTo(REGION_NAME3);
+        softly.assertThat(results.get(2).getCountry()).isEqualTo(REGION_COUNTRY3);
+
+        softly.assertAll();
+    }
+
+    @Test
+    void shouldRetrieveRegionTypeMap() {
+        when(regionRepository.findAll()).thenReturn(REGIONS);
+        final Map<Integer, uk.gov.hmcts.dts.fact.entity.Region> results = adminRegionService.getRegionMap();
+
+        final SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(results).hasSize(3);
+        softly.assertThat(results.get(REGION_ID1)).isEqualTo(REGION1);
+        softly.assertThat(results.get(REGION_ID2)).isEqualTo(REGION2);
+        softly.assertThat(results.get(REGION_ID3)).isEqualTo(REGION3);
+        softly.assertAll();
+    }
+}
