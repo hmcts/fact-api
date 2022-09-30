d00d2926f48bb593d797739237700575fc28185e
Imran Ali <Imran.Ali@justice.gov.uk>
2022-09-30 03:54:58 +0100

    FACT-1178 notFoundExceptionHandler

--- src/main/java/uk/gov/hmcts/dts/fact/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/dts/fact/exception/GlobalControllerExceptionHandler.java
@@ -1,19 +1,29 @@
 package uk.gov.hmcts.dts.fact.exception;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
 import java.util.List;
 
 import static org.springframework.http.ResponseEntity.badRequest;
 
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
-    @ExceptionHandler(NotFoundException.class)
-    ResponseEntity<String> notFoundExceptionHandler(final NotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+
+    @ExceptionHandler(value = {NotFoundException.class})
+    ResponseEntity<String> notFoundExceptionHandler(final NotFoundException ex) throws JsonProcessingException
+    {
+        HashMap<String, String> error = new HashMap<>();
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Content-Type", "application/json");
+        error.put("message", ex.getMessage());
+        return new ResponseEntity<>(new ObjectMapper().writeValueAsString(error), responseHeaders, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InvalidPostcodeException.class)
