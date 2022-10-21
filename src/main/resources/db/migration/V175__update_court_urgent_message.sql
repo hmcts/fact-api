932f467d11b306d668a0f1dfcdb64dcb591b1ab8
Majid-justice <81159704+Majid-justice@users.noreply.github.com>
2022-10-21 09:44:25 +0100

    remove court urgent message (#720)

---
+++ src/main/resources/db/migration/V175__update_court_urgent_message.sql
@@ -0,0 +1,4 @@
+UPDATE public.search_court
+SET alert = '',
+alert_cy = ''
+where alert = 'Following the death of Her Majesty The Queen, most court and tribunal hearings will not take place on 19 September 2022, the day of the State Funeral. <a href="https://www.gov.uk/government/news/courts-and-tribunals-arrangements-for-the-queens-state-funeral" rel="nofollow" data-mce-href="https://www.gov.uk/government/news/courts-and-tribunals-arrangements-for-the-queens-state-funeral">Find out more information about court and tribunal arrangements at this time</a>'
