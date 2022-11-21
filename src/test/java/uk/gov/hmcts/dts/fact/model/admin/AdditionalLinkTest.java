e15b067e68d5e569b9609201fc91b2610c5f1eb2
majid.najib <majid.najib@justice.gov.uk>
2022-11-21 16:11:23 +0000

    fix checkstyle issues

--- src/test/java/uk/gov/hmcts/dts/fact/model/admin/AdditionalLinkTest.java
+++ src/test/java/uk/gov/hmcts/dts/fact/model/admin/AdditionalLinkTest.java
@@ -7,11 +7,12 @@ class AdditionalLinkTest {
     private static final String TEST_URL = "https://www.additional-link.com";
     private static final String TEST_DESCRIPTION = "description";
     private static final String TEST_DESCRIPTION_CY = "description cy";
+
     @Test
     void testCreation() {
         final uk.gov.hmcts.dts.fact.entity.AdditionalLink additionalLinkEntity = new uk.gov.hmcts.dts.fact.entity.AdditionalLink(TEST_URL, TEST_DESCRIPTION, TEST_DESCRIPTION_CY);
-
         final AdditionalLink result = new AdditionalLink(additionalLinkEntity);
+
         SoftAssertions softly = new SoftAssertions();
         softly.assertThat(result.getUrl()).isEqualTo(TEST_URL);
         softly.assertThat(result.getDisplayName()).isEqualTo(TEST_DESCRIPTION);
