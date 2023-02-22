02a7158c01a9f4eee4f8a4bc309194c17e5e72a4
majid.najib <majid.najib@justice.gov.uk>
2023-02-22 15:41:50 +0000

    update medway county lat and lon values

--- src/main/resources/db/migration/V188__update_medway_lat_and _lon.sql
+++ src/main/resources/db/migration/V188__update_medway_lat_and _lon.sql
@@ -1,1 +1,1 @@
-UPDATE search_court set lat = 51.388631277795355 , lon = 0.523719590860512 WHERE slug = 'medway-county-court-and-family-court';
+UPDATE search_court set lat = 51.38856122180704 , lon = 0.5236820006263685 WHERE slug = 'medway-county-court-and-family-court';
