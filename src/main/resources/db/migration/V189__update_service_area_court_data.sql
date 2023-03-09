a0454a5e0df1a2863e95c9cea539c9670b44e080
majid.najib <majid.najib@justice.gov.uk>
2023-03-09 11:32:51 +0000

    update

--- src/main/resources/db/migration/V189__update_service_area_court_data.sql
+++ src/main/resources/db/migration/V189__update_service_area_court_data.sql
@@ -1,6 +1,4 @@
 --to fix data issue for fact frontend failing functional tests--
 DELETE FROM public.search_serviceareacourt
-  WHERE court_id = (SELECT id
-                         FROM search_court
-                         WHERE slug = 'pocock-street-tribunal-hearing-centre');
+  WHERE court_id = 1479504;
 
