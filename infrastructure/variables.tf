19e242dc738f8cecf6a8f53a86cbce731ee5d89c
majid.najib <majid.najib@justice.gov.uk>
2023-03-09 14:12:30 +0000

    update

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -14,6 +14,8 @@ variable "env" {}
 
 variable "subscription" {}
 
+variable "deployment_namespace" {}
+
 variable "common_tags" {
   type = map
 }
@@ -24,6 +26,8 @@ variable "jenkins_AAD_objectId" {
   type = string
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
+
+variable "ilbIp"{}
 
 variable "managed_identity_object_id" {
   default = ""
