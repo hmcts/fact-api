445c47d7856d4fbe11ff99ab68bd260b50555342
Adam Stevenson <addstevenson@hotmail.com>
2023-03-15 10:11:36 +0000

    fix pipeline (#758)

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -21,11 +21,14 @@ variable "common_tags" {
 
 variable "tenant_id" {}
 
+variable "ilbIp" {
+  default = ""
+}
+
 variable "jenkins_AAD_objectId" {
   type = string
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
-
 
 variable "managed_identity_object_id" {
   default = ""
