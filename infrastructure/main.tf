655a5a1681b2fd743126e0efa08e79cef2e0ebd2
amstevenson <addstevenson@hotmail.com>
2023-10-25 14:10:10 +0100

    adding cve fixes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,13 @@ provider "azurerm" {
   features {}
 }
 
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  alias                      = "postgres_network"
+  subscription_id            = var.aks_subscription_id
+}
+
 locals {
   vault_name = "${var.product}-${var.env}"
   resource_group_name = "${var.product}-${var.env}"
@@ -64,4 +71,36 @@ module "fact-database" {
   storage_mb         = "51200"
   common_tags        = var.common_tags
   subscription       = var.subscription
+}
+
+locals {
+  vault_name = "${var.product}-${var.env}"
+  resource_group_name = "${var.product}-${var.env}"
+  db_host_name = "${var.product}-${var.component}-flexible-postgres-db-v15"
+  db_name = replace(var.component, "-", "")
+  postgresql_user = "${local.db_name}_user"
+}
+
+module "postgresql" {
+  providers = {
+    azurerm.postgres_network = azurerm.postgres_network
+  }
+
+  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  name                 = local.db_host_name
+  product              = var.product
+  component            = var.component
+  location             = var.location
+  env                  = var.env
+  pgsql_admin_username = local.postgresql_user
+  pgsql_databases = [
+    {
+      name : "fact"
+    }
+  ]
+  common_tags   = var.common_tags
+  business_area = "cft"
+  pgsql_version = "15"
+
+  admin_user_object_id = var.jenkins_AAD_objectId
 }
--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -2,9 +2,9 @@ terraform {
   backend "azurerm" {}
 
   required_providers {
-       azurerm = {
-         source  = "hashicorp/azurerm"
-         version = "3.40"
-       }
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.76.0"
     }
+  }
 }
