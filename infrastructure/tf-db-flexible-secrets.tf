ebba693470dc3d6cef8ae7c675e781af9e7adaea
amstevenson <addstevenson@hotmail.com>
2023-10-25 13:17:50 +0100

    adding flex db for fact

---
+++ infrastructure/tf-db-flexible-secrets.tf
@@ -0,0 +1,39 @@
+locals {
+  flexible_secret_prefix = "${var.component}-POSTGRES-FLEXIBLE"
+
+  flexible_secrets = [
+    {
+      name_suffix = "PASS"
+      value       = module.postgresql.password
+    },
+    {
+      name_suffix = "HOST"
+      value       = module.postgresql.fqdn
+    },
+    {
+      name_suffix = "USER"
+      value       = module.postgresql.username
+    },
+    {
+      name_suffix = "PORT"
+      value       = "5432"
+    },
+    {
+      name_suffix = "DATABASE"
+      value       = local.db_name
+    }
+  ]
+
+}
+
+resource "azurerm_key_vault_secret" "flexible_secret" {
+  for_each     = { for secret in local.flexible_secrets : secret.name_suffix => secret }
+  key_vault_id = data.azurerm_key_vault.fact_key_vault.id
+  name         = "${local.flexible_secret_prefix}-${each.value.name_suffix}"
+  value        = each.value.value
+  tags = merge(var.common_tags, {
+    "source" : "${var.component} PostgreSQL"
+  })
+  content_type    = ""
+  expiration_date = timeadd(timestamp(), "17520h")
+}
