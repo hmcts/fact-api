23f1c50ef4620b042f3ea5b26a9b6a9c8bd34776
majid.najib <majid.najib@justice.gov.uk>
2023-10-26 11:10:48 +0100

    update flyway script

--- src/main/resources/db/migration/V197__make_maintenance_enforcement_centre_not_in_person.sql
+++ src/main/resources/db/migration/V197__make_maintenance_enforcement_centre_not_in_person.sql
@@ -15,5 +15,40 @@ VALUES
     false,
     false
   );
+INSERT INTO public.search_servicecentre
+(court_id, intro_paragraph, id)
+VALUES (
+         (
+           SELECT id
+           FROM public.search_court
+           WHERE slug = 'maintenance-enforcement-business-centre-england' or slug = 'maintenance-enforcement-business-centre'
+         ),
+          'This location services all of England and Wales for Reciprocal Enforcement of Maintenance Orders. We do not provide an in-person service. ',
+         DEFAULT
+       );
 
 
+INSERT INTO public.search_additionallink
+(id, url, description)
+VALUES (
+      DEFAULT,
+      'https://www.gov.uk/child-maintenance-if-one-parent-lives-abroad/other-partner-lives-abroad',
+      'Child maintenance if a parent lives abroad'
+  );
+
+INSERT INTO public.search_courtadditionallink
+(id, court_id, additional_link_id, sort)
+VALUES (
+      DEFAULT,
+  (
+  SELECT id
+  FROM public.search_court
+  WHERE slug = 'maintenance-enforcement-business-centre-england' or slug = 'maintenance-enforcement-business-centre'
+  ),
+  (
+  SELECT id
+  FROM public.search_additionallink
+  WHERE description = 'Child maintenance if a parent lives abroad'
+  ),
+  0
+  )
