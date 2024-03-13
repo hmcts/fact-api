dc7c74246c376fdb43cdfbe7e5c44cf42266f02c
Imran Ali <Imran.Ali@justice.gov.uk>
2024-03-13 12:33:33 +0000

    added docstrings for exceptions

--- src/main/java/uk/gov/hmcts/dts/fact/exception/MapitUsageException.java
+++ src/main/java/uk/gov/hmcts/dts/fact/exception/MapitUsageException.java
@@ -4,6 +4,9 @@ public class MapitUsageException extends RuntimeException {
     private static final long serialVersionUID = 6751592734307424106L;
     private static final String USAGE_LIMIT_REACHED = "Usage limit reached.";
 
+    /**
+     * Constructs a new mapit usage exception with the specified detail message.
+     */
     public MapitUsageException() {
         super(USAGE_LIMIT_REACHED);
     }
--- src/main/java/uk/gov/hmcts/dts/fact/exception/PendingMigrationScriptException.java
+++ src/main/java/uk/gov/hmcts/dts/fact/exception/PendingMigrationScriptException.java
@@ -4,6 +4,11 @@ public class PendingMigrationScriptException extends RuntimeException {
 
     private static final long serialVersionUID = -7544166728568238022L;
 
+    /**
+     * Constructs a new pending migration script exception with the specified detail message.
+     *
+     * @param script the detail message
+     */
     public PendingMigrationScriptException(String script) {
         super("Found migration not yet applied: " + script);
     }
