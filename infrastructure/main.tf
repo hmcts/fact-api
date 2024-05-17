3c10621a5daa9898c1d4a41988ac6e213a30c95d
Ruth Bovell <ruthb111@outlook.com>
2024-05-17 13:33:52 +0100

    - force user permissions and force schema ownership

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -53,4 +53,7 @@ module "postgresql" {
   pgsql_version = "15"
 
   admin_user_object_id = var.jenkins_AAD_objectId
+  force_user_permissions_trigger = "1"
+  enable_schema_ownership = true
+  force_schema_ownership_trigger = "true"
 }
