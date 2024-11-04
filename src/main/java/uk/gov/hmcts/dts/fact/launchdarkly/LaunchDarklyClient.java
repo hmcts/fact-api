9d1868492d52fbd2c149b6fcbc287786ad824399
majid.najib <majid.najib@justice.gov.uk>
2024-11-04 18:03:16 +0000

    update packages

--- src/main/java/uk/gov/hmcts/dts/fact/launchdarkly/LaunchDarklyClient.java
+++ src/main/java/uk/gov/hmcts/dts/fact/launchdarkly/LaunchDarklyClient.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.dts.fact.launchdarkly;
 
-import com.launchdarkly.sdk.LDUser;
+import com.launchdarkly.sdk.LDContext;
 import com.launchdarkly.sdk.server.interfaces.LDClientInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,9 +11,8 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class LaunchDarklyClient {
-    public static final LDUser FACT_API_USER = new LDUser.Builder("fact-api")
-        .anonymous(true)
-        .build();
+
+    public final LDContext factApiContext;
 
     private final LDClientInterface internalClient;
 
@@ -31,6 +30,7 @@ public class LaunchDarklyClient {
         @Value("${launchdarkly.offline-mode:false}") Boolean offlineMode
     ) {
         this.internalClient = launchDarklyClientFactory.create(sdkKey, offlineMode);
+        this.factApiContext =  LDContext.builder(sdkKey).build();
     }
 
     /**
@@ -40,7 +40,7 @@ public class LaunchDarklyClient {
      * @return true if the feature is enabled, false otherwise
      */
     public boolean isFeatureEnabled(String feature) {
-        return internalClient.boolVariation(feature, LaunchDarklyClient.FACT_API_USER, false);
+        return internalClient.boolVariation(feature, factApiContext, false);
     }
 
     /**
@@ -50,7 +50,7 @@ public class LaunchDarklyClient {
      * @param user the user to check the feature for
      * @return true if the feature is enabled, false otherwise
      */
-    public boolean isFeatureEnabled(String feature, LDUser user) {
+    public boolean isFeatureEnabled(String feature, LDContext user) {
         return internalClient.boolVariation(feature, user, false);
     }
 }
