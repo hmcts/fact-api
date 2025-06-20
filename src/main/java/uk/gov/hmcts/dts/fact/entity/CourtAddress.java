016eca9a857bec67b2f5855e227c204b7a361dc4
Imran Ali <101260174+justiceia@users.noreply.github.com>
2025-06-20 15:30:53 +0100

    FACT-2446 added fix for TransientObjectException error (#859)
    * FACT-2446 added fix for TransientObjectException error
    
    * fix

--- src/main/java/uk/gov/hmcts/dts/fact/entity/CourtAddress.java
+++ src/main/java/uk/gov/hmcts/dts/fact/entity/CourtAddress.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.dts.fact.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -47,8 +48,8 @@ public class CourtAddress {
     @JoinColumn(name = "county_id")
     private County county;
     private String postcode;
-    @OneToMany
-    @JoinColumn(name = "address_id")
+
+    @OneToMany(mappedBy = "address", cascade = CascadeType.PERSIST)
     private List<CourtSecondaryAddressType> courtSecondaryAddressType;
 
     private Integer sortOrder;
