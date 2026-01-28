ceed14943107e43bddb9db5b979b2c4bce84f793
Josh <23316525+josh-uk@users.noreply.github.com>
2026-01-28 13:27:39 +0000

    Update main.tf (#973)
    Trigger DB to assign reader permissions.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -53,5 +53,5 @@ module "postgresql" {
   pgsql_version = "15"
 
   admin_user_object_id           = var.jenkins_AAD_objectId
-  force_user_permissions_trigger = "1"
+  force_user_permissions_trigger = "2"
 }
