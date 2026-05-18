2acbfdddb939aabc279bd86f6cc8b7162638c393
Jon <hurricanepilot@gmail.com>
2026-05-18 13:58:09 +0100

    FACT-2725 - IDAM migration updates (#994)
    * update IDAM bearer token validation url
    
    * update oauth token retrieval to use non-deprecated endpoint
    
    * only retrieve oauth tokens at start of run
    
    * workaround for test where superadmin owns the lock
    
    * empty commit

--- src/functionalTest/java/uk/gov/hmcts/dts/fact/util/OAuthClient.java
+++ src/functionalTest/java/uk/gov/hmcts/dts/fact/util/OAuthClient.java
@@ -3,8 +3,6 @@ package uk.gov.hmcts.dts.fact.util;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Base64;
-
 import static io.restassured.RestAssured.given;
 
 @Component
@@ -16,10 +14,7 @@ public class OAuthClient {
     @Value("${OAUTH_SECRET:}")
     private String clientSecret;
 
-    @Value("${OAUTH_REDIRECT:http://localhost:3300/oauth2/callback}")
-    private String redirectUri;
-
-    @Value("${OAUTH_PROVIDER_URL:https://idam-api.aat.platform.hmcts.net}")
+    @Value("${OAUTH_PROVIDER_URL:https://idam-web-public.aat.platform.hmcts.net}")
     private String providerUrl;
 
     @Value("${OAUTH_USER}")
@@ -54,11 +49,11 @@ public class OAuthClient {
     }
 
     public String generateClientToken(String userName, String password) {
-        String code = generateClientCode(userName, password);
         String token = given()
             .relaxedHTTPSValidation()
             .baseUri(providerUrl)
-            .post("/oauth2/token" + "?code=" + code + "&client_secret=" + clientSecret + "&client_id=" + clientId + "&redirect_uri=" + redirectUri + "&grant_type=authorization_code")
+            .post("/o/token?grant_type=password&username=" + userName + "&password=" + password
+                      + "&client_id=" + clientId + "&client_secret=" + clientSecret + "&scope=openid profile roles")
             .body()
             .jsonPath()
             .get("id_token");
@@ -66,45 +61,14 @@ public class OAuthClient {
         if (token == null) {
             throw new AuthException(
                 String.format(
-                    "Unable to get token with %s %s %s %s %s",
+                    "Unable to get token with %s %s %s",
                     providerUrl,
                     clientId,
-                    clientSecret,
-                    redirectUri,
-                    code
+                    clientSecret
                 )
             );
         }
 
         return token;
-    }
-
-
-    private String generateClientCode(String userName, String password) {
-        final String encoded = Base64.getEncoder().encodeToString((userName + ":" + password).getBytes());
-
-        String code = given()
-            .relaxedHTTPSValidation()
-            .baseUri(providerUrl)
-            .header("Authorization", "Basic " + encoded)
-            .post("/oauth2/authorize?response_type=code&client_id=" + clientId + "&redirect_uri=" + redirectUri)
-            .body()
-            .jsonPath()
-            .get("code");
-
-        if (code == null) {
-            throw new AuthException(
-                String.format(
-                    "Unable to get auth code with %s %s %s %s %s",
-                    providerUrl,
-                    clientId,
-                    redirectUri,
-                    userName,
-                    password
-                )
-            );
-        }
-
-        return code;
     }
 }
